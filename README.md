e4ad3e5d292f5ef4d133d8bf723265f8d4d9c655
Filippo Brizzi <f.brizzi@funkflow.com>
2024-01-15 15:03:56 +0000

    add docker dev env

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # eolo
+
+## Build
+
+The best way to build eolo is to do it inside the docker container provided in the `docker` folder. You can build the container with `build.sh` and start it using `run.sh`.
---
+++ docker/build.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+source ./version.sh
+
+
+ARCH=$(uname -m)
+
+if [ "${ARCH}" == "x86_64" ]; then
+    BASE_IMAGE="nvidia/opengl:1.0-glvnd-runtime-ubuntu22.04"
+else
+    BASE_IMAGE="arm64v8/ubuntu:22.04"
+fi
+
+ncores=$(cat /proc/cpuinfo | grep processor | wc -l)
+
+docker build -t ${IMAGE}:${VERSION} -f Dockerfile --cpuset-cpus "0-$ncores" --build-arg BASE_IMAGE=${BASE_IMAGE} .
