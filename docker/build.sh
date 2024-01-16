8f5877cf0930a8cf6d9e765435ba0d15cb48706e
Filippo Brizzi <f.brizzi@funkflow.com>
2024-01-16 11:21:14 +0000

    fix docker image

--- docker/build.sh
+++ docker/build.sh
@@ -6,6 +6,7 @@ source ./version.sh
 ARCH=$(uname -m)
 
 if [ "${ARCH}" == "x86_64" ]; then
+    # see https://hub.docker.com/r/nvidia/opengl/tags
     BASE_IMAGE="nvidia/opengl:1.0-glvnd-runtime-ubuntu22.04"
 else
     BASE_IMAGE="arm64v8/ubuntu:22.04"
