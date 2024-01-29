d696b7d0ccffbbe1a93f989e2aadf6999a6fa7ae
Filippo Brizzi <f.brizzi@funkflow.com>
2024-01-29 15:01:05 +0000

    testing github action

--- modules/utils/src/version_impl.h
+++ modules/utils/src/version_impl.h
@@ -17,8 +17,8 @@ static constexpr std::uint8_t VERSION_MAJOR = 0;
 static constexpr std::uint8_t VERSION_MINOR = 0;
 static constexpr std::uint16_t VERSION_PATCH = 0;
 
-static constexpr std::string_view REPO_BRANCH = "main";
+static constexpr std::string_view REPO_BRANCH = "fix/github";
 static constexpr std::string_view BUILD_PROFILE = "RelWithDebInfo";
-static constexpr std::string_view REPO_HASH = "d33be56";
+static constexpr std::string_view REPO_HASH = "9a4e832";
 
 } // namespace eolo::utils
