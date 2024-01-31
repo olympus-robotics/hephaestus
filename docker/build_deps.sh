2b33533904d4bf64e012a9e9b798de80cee2db9c
Filippo Brizzi <filippobrizzi@gmail.com>
2024-01-31 15:00:47 +0000

    working maybe

--- docker/build_deps.sh
+++ docker/build_deps.sh
@@ -9,6 +9,8 @@ SUFFIX=deps
 
 pushd ../
 
-docker build . -t ${IMAGE}_${SUFFIX}:${VERSION} -f docker/Dockerfile_deps --cpuset-cpus "0-$ncores" --build-arg BASE_IMAGE=${IMAGE}:${VERSION} --build-arg CODEBASE=${CODEBASE} --progress=plain --no-cache
+BASE_IMAGE=ghcr.io/filippobrizzi/${IMAGE}:${VERSION}
+docker pull ${BASE_IMAGE}
+docker build . -t ${IMAGE}_${SUFFIX}:${VERSION} -f docker/Dockerfile_deps --cpuset-cpus "0-$ncores" --build-arg BASE_IMAGE=${BASE_IMAGE} --tag ${IMAGE}_${SUFFIX}:latest
 
 popd
