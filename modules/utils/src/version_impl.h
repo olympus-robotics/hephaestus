2474d2dba5b07cb2cba78083fa9bdc0d76d2016c
Filippo Brizzi <filippobrizzi@gmail.com>
2024-02-14 18:28:45 +0000

    work

--- modules/utils/src/version_impl.h
+++ modules/utils/src/version_impl.h
@@ -18,7 +18,7 @@ static constexpr std::uint8_t VERSION_MINOR = 0;
 static constexpr std::uint16_t VERSION_PATCH = 1;
 
 static constexpr std::string_view REPO_BRANCH = "fix_cmake_format";
-static constexpr std::string_view BUILD_PROFILE = "RelWithDebInfo";
-static constexpr std::string_view REPO_HASH = "207ae9b";
+static constexpr std::string_view BUILD_PROFILE = "Release";
+static constexpr std::string_view REPO_HASH = "35578db";
 
 } // namespace eolo::utils
