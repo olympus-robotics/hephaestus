be6d74986ccf444f9444a072d5cb29fb0a42f287
Florian Tschopp <fts@voliro.com>
2024-03-19 09:29:25 +0100

    debug

--- modules/utils/include/hephaestus/utils/concepts.h
+++ modules/utils/include/hephaestus/utils/concepts.h
@@ -10,7 +10,9 @@
 namespace heph {
 
 template <typename T>
-concept ScalarType = requires(T a) { std::is_scalar_v<T>; };
+concept ScalarType = requires(T a) {
+  std::is_scalar_v<T>;
+};
 
 /// Types that are convertable to and from a string
 template <typename T>
