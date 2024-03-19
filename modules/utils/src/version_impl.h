65db09609446f3f66475ad3db1af8f8e3de77b6a
Florian Tschopp <fts@voliro.com>
2024-03-19 16:32:29 +0100

    fix

--- modules/utils/src/version_impl.h
+++ modules/utils/src/version_impl.h
@@ -19,6 +19,6 @@ static constexpr std::uint16_t VERSION_PATCH = 1;
 
 static constexpr std::string_view REPO_BRANCH = "devel/ament";
 static constexpr std::string_view BUILD_PROFILE = "RelWithDebInfo";
-static constexpr std::string_view REPO_HASH = "d99f276";
+static constexpr std::string_view REPO_HASH = "ec6db57";
 
 } // namespace heph::utils
