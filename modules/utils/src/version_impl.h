c4ba30a1de56084621af2393989ed7ffdc45152f
Filippo Brizzi <filippobrizzi@gmail.com>
2024-03-25 13:58:42 +0000

    upgrade zenohc version

--- modules/utils/src/version_impl.h
+++ modules/utils/src/version_impl.h
@@ -17,8 +17,8 @@ static constexpr std::uint8_t VERSION_MAJOR = 0;
 static constexpr std::uint8_t VERSION_MINOR = 0;
 static constexpr std::uint16_t VERSION_PATCH = 1;
 
-static constexpr std::string_view REPO_BRANCH = "devel/ament";
+static constexpr std::string_view REPO_BRANCH = "main";
 static constexpr std::string_view BUILD_PROFILE = "RelWithDebInfo";
-static constexpr std::string_view REPO_HASH = "65db096";
+static constexpr std::string_view REPO_HASH = "28bee7f";
 
 } // namespace heph::utils
