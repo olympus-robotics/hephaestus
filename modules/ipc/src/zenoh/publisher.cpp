004e611aa32cafc8e3604dccf72b62e30ba7c1cc
Florian Tschopp <fts@voliro.com>
2024-04-03 18:40:58 +0200

    fixes

--- modules/ipc/src/zenoh/publisher.cpp
+++ modules/ipc/src/zenoh/publisher.cpp
@@ -91,7 +91,7 @@ void Publisher::createTypeInfoService() {
     (void)request;
     return type_info_json;
   };
-  auto type_service_topic = getTypeInfoServiceTopic(topic_config_.name);
+  auto type_service_topic = TopicConfig{ .name = getTypeInfoServiceTopic(topic_config_.name) };
   type_service_ = std::make_unique<Service<std::string, std::string>>(session_, type_service_topic,
                                                                       std::move(type_info_callback));
 }
