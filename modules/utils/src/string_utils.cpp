7145925afc9d6771a0b7124268120dff80e90c86
Filippo Brizzi <filippobrizzi@gmail.com>
2024-04-05 12:48:04 +0000

    fixed

--- modules/utils/src/string_utils.cpp
+++ modules/utils/src/string_utils.cpp
@@ -1,13 +1,3 @@
 #include "hephaestus/utils/string_utils.h"
 
-namespace heph::utils {
-
-// constexpr auto truncate(std::string_view str, std::string_view start_token,
-//                         std::string_view end_token /* = std::string_view("") */) -> std::string_view {
-//   const auto start_pos = str.find(start_token);
-//   const auto end_pos = end_token.empty() ? std::string_view::npos : str.find(end_token);
-//   return (start_pos != std::string_view::npos) ? str.substr(start_pos, end_pos - start_pos) :
-//                                                  str.substr(0, end_pos);
-// }
-
-}  // namespace heph::utils
+namespace heph::utils {}  // namespace heph::utils
