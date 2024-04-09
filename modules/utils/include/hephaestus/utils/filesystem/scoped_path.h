6ae01e9f36db088c806ac4593d26db0bf39c5348
Filippo Brizzi <filippobrizzi@gmail.com>
2024-04-09 16:07:06 +0200

    File utils (#44)
    # Description
    Add utils to read and write to files
    
    - New feature (non-breaking change which adds functionality)

---
+++ modules/utils/include/hephaestus/utils/filesystem/scoped_path.h
@@ -0,0 +1,38 @@
+#pragma once
+#include <filesystem>
+
+namespace heph::utils::filesystem {
+
+/// This class allows to create a file that is removed when the class goes out of scope (RAII).
+/// This is very useful in tests to avoid having dangling files.
+class ScopedPath {
+public:
+  ~ScopedPath();
+  ScopedPath(const ScopedPath&) = delete;
+  ScopedPath(ScopedPath&&) = default;
+  auto operator=(const ScopedPath&) -> ScopedPath& = delete;
+  auto operator=(ScopedPath&&) -> ScopedPath& = default;
+
+  [[nodiscard]] static auto createFile() -> ScopedPath;
+
+  [[nodiscard]] static auto createDir() -> ScopedPath;
+
+  operator std::filesystem::path() const;  // NOLINT(google-explicit-conversion,
+                                           // google-explicit-constructor)
+
+  operator std::string() const;  // NOLINT(google-explicit-conversion,
+                                 // google-explicit-constructor)
+
+  operator std::string_view() const;  // NOLINT(google-explicit-conversion,
+                                      // google-explicit-constructor)
+
+private:
+  explicit ScopedPath(std::filesystem::path path);
+
+  [[nodiscard]] static auto randomString() -> std::string;
+
+  std::filesystem::path path_;
+  std::string path_str_;
+};
+
+}  // namespace heph::utils::filesystem
