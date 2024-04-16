c4335580170f000ebcde1dc7e5a81c42751c0860
Florian Tschopp <fts@voliro.com>
2024-04-16 17:40:25 +0200

    cleanup

--- modules/ipc/apps/zenoh_topic_echo.cpp
+++ modules/ipc/apps/zenoh_topic_echo.cpp
@@ -2,15 +2,14 @@
 // Copyright (C) 2023-2024 HEPHAESTUS Contributor
 //=================================================================================================
 
-#include <chrono>
 #include <csignal>
 #include <cstdlib>
 #include <string>
-#include <thread>
 
 #include <fmt/chrono.h>
 #include <fmt/core.h>
 #include <fmt/format.h>
+#include <nlohmann/json.hpp>
 #include <zenoh.h>
 #include <zenohc.hxx>
 
@@ -23,6 +22,29 @@
 #include "hephaestus/utils/exception.h"
 #include "zenoh_program_options.h"
 
+namespace {
+constexpr auto MAX_ARRAY_LENGTH = 100;
+
+void truncateJsonItem(nlohmann::json& obj, size_t max_length) {
+  if (obj.is_object()) {
+    for (auto& [key, value] : obj.items()) {
+      truncateJsonItem(value, max_length);
+    }
+  } else {
+    if (obj.dump().length() > max_length) {
+      obj = "<long item>";
+    }
+  }
+}
+
+void truncateLongItems(std::string& msg_json, bool noarr, size_t max_length) {
+  if (noarr && msg_json.size() > MAX_ARRAY_LENGTH) {
+    nlohmann::json json_obj = nlohmann::json::parse(msg_json);
+    truncateJsonItem(json_obj, max_length);
+    msg_json = json_obj.dump();
+  }
+}
+
 [[nodiscard]] auto getTopicTypeInfo(heph::ipc::zenoh::Session& session,
                                     const std::string& topic) -> heph::serdes::TypeInfo {
   auto service_topic = heph::ipc::getTypeInfoServiceTopic(topic);
@@ -34,6 +56,7 @@
 
   return heph::serdes::TypeInfo::fromJson(response.front().value);
 }
+}  // namespace
 
 std::atomic_flag stop_flag = false;  // NOLINT(cppcoreguidelines-avoid-non-const-global-variables)
 auto signalHandler(int /*unused*/) -> void {
@@ -47,6 +70,7 @@ auto main(int argc, const char* argv[]) -> int {
     (void)signal(SIGTERM, signalHandler);
 
     auto desc = getProgramDescription("Echo the data from a topic to the console.");
+    desc.defineFlag("noarr", "Disable to print long arrays");
     const auto args = std::move(desc).parse(argc, argv);
 
     auto [session_config, topic_config] = parseArgs(args);
@@ -61,9 +85,11 @@ auto main(int argc, const char* argv[]) -> int {
     heph::serdes::DynamicDeserializer dynamic_deserializer;
     dynamic_deserializer.registerSchema(type_info);
 
-    auto cb = [&dynamic_deserializer, type = type_info.name](const heph::ipc::MessageMetadata& metadata,
-                                                             std::span<const std::byte> buffer) mutable {
+    auto cb = [&dynamic_deserializer, type = type_info.name, noarr = args.getOption<bool>("noarr")](
+                  [[maybe_unused]] const heph::ipc::MessageMetadata& metadata,
+                  std::span<const std::byte> buffer) mutable {
       auto msg_json = dynamic_deserializer.toJson(type, buffer);
+      truncateLongItems(msg_json, noarr, MAX_ARRAY_LENGTH);
       fmt::println("From: {}. Topic: {} - {}", metadata.sender_id, metadata.topic, msg_json);
     };
 
