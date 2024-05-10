8ad59dc3f5e6636671af99e404b824ea829cdb88
Lorenz Hruby <lorenz.hruby@gmx.at>
2024-05-10 09:53:37 +0200

    Feat/Create random module. (#61)
    # Description
    The **random** module contains generic functionality to generate random
    data, as it is often used in unit tests.
    * a random number generator (RNG) which is pre-configured to be
    deterministic or not in order to enforce (non-)determinism in all tests
    * a method `randomT<T>(std::mt19937_64& mt) -> T` which can be used to
    create a random element of a primitive, custom or container type
    
    
    ## Type of change
    - New feature (non-breaking change which adds functionality)
    
    ## Checklist before requesting a review
    - [x] I have performed a self-review of my code.
    - [x] If it is a core feature, I have added thorough tests.
    - [ ] If this is a new component I have added examples.
    - [x] I updated the README and related documentation.
    
    ---------
    
    Co-authored-by: Lorenz Hruby <hruby@filics.eu>

---
+++ modules/random/src/random_generator.cpp
@@ -0,0 +1,39 @@
+//=================================================================================================
+// Copyright (C) 2023-2024 HEPHAESTUS Contributors
+//=================================================================================================
+
+#include "hephaestus/random/random_generator.h"
+
+#include <cstdint>
+#include <random>
+
+#include <fmt/core.h>
+
+namespace heph::random {
+
+namespace {
+
+[[nodiscard]] auto getSeed(bool is_deterministic) -> uint64_t {
+  if (is_deterministic) {
+    static constexpr uint64_t FIXED_SEED = 42;
+    return FIXED_SEED;
+  }
+
+  const auto seed = std::random_device{}();
+  fmt::println("test_helper RNG seed: {}", seed);
+  return seed;
+}
+
+}  // namespace
+
+auto createRNG(bool is_deterministic) -> std::mt19937_64 {
+  const auto seed = getSeed(is_deterministic);
+  return std::mt19937_64{ seed };
+}
+
+auto createPairOfIdenticalRNGs(bool is_deterministic) -> std::pair<std::mt19937_64, std::mt19937_64> {
+  const auto seed = getSeed(is_deterministic);
+  return { std::mt19937_64{ seed }, std::mt19937_64{ seed } };
+}
+
+}  // namespace heph::random
