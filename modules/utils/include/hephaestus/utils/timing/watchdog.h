c571a8052243b29a85c704887b5527ff8cbed9aa
Filippo Brizzi <filippobrizzi@gmail.com>
2024-06-21 14:04:41 +0200

    Watchdog timer (#82)
    # Description
    Create a watchdog timer. 
    
    The timer call the given callback every `period` time.
    A function `pat()` is also provided to implement a dead-man switch.
    
    Example use case:
    In a motor controller, if no command is received within the time period,
    stop the motors.

---
+++ modules/utils/include/hephaestus/utils/timing/watchdog.h
@@ -0,0 +1,44 @@
+//=================================================================================================
+// Copyright (C) 2023-2024 HEPHAESTUS Contributors
+//=================================================================================================
+
+#pragma once
+
+#include <chrono>
+#include <functional>
+#include <thread>
+
+namespace heph::utils::timing {
+
+class WatchdogTimer {
+public:
+  using Callback = std::function<void()>;
+
+  /// Start the watchdog timer with the specified period and callback.
+  /// Once the timer starts it will loop indefinitely, calling the callback ever `period` milliseconds.
+  void start(std::chrono::milliseconds period, Callback&& callback);
+
+  /// Stop the timer.
+  void stop();
+
+  /// Pat the watchdog. If called the timer will not call the callback for the next period.
+  /// This can be used to implement a dead-man switch.
+  void pat();
+
+private:
+  void loop();
+
+private:
+  std::chrono::system_clock::time_point start_timestamp_;
+  uint64_t fire_count_{ 0 };
+  std::chrono::milliseconds period_{ 0 };
+  Callback callback_{ nullptr };
+
+  std::atomic<bool> running_{ false };
+  std::atomic<bool> pat_{ false };
+  std::mutex mutex_;
+  std::condition_variable condition_;
+  std::thread thread_;
+};
+
+}  // namespace heph::utils::timing
--- modules/utils/include/hephaestus/utils/utils.h
+++ modules/utils/include/hephaestus/utils/utils.h
@@ -5,7 +5,6 @@
 #pragma once
 
 #include <memory>
-#include <sstream>
 #include <string>
 
 #include <cxxabi.h>
