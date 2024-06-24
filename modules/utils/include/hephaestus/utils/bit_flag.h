ca984365942be16ed5b2ed90101edcc8290f341b
Filippo Brizzi <filippobrizzi@gmail.com>
2024-06-24 06:29:38 +0200

    Add BitFlag class (#83)
    # Description
    Add a new class `BitFlag<EnumT>` that allows to create bit flags.
    
    The usage is visible from the unit tests and the class documentation. If
    something is not clear, please let me know I will improve the feedback.

---
+++ modules/utils/include/hephaestus/utils/bit_flag.h
@@ -0,0 +1,109 @@
+//=================================================================================================
+// Copyright (C) 2023-2024 HEPHAESTUS Contributors
+//=================================================================================================
+
+#pragma once
+
+#include <absl/synchronization/mutex.h>
+#include <fmt/core.h>
+#include <magic_enum.hpp>
+#include <magic_enum_all.hpp>
+
+namespace heph::utils {
+
+namespace internal {
+template <typename EnumT>
+constexpr auto checkEnumValuesArePowerOf2() -> bool {
+  constexpr auto POWER_OF_TWO = magic_enum::enum_for_each<EnumT>([](auto val) {
+    constexpr EnumT VALUE = val;
+    constexpr auto VALUE_T = static_cast<std::underlying_type_t<EnumT>>(VALUE);
+    if constexpr (VALUE_T == 0) {
+      return true;  // 0 is not power of 2
+    }
+
+    return (VALUE_T & static_cast<std::underlying_type_t<EnumT>>(VALUE_T - 1)) ==
+           0;  // check if VALUE_T is power of 2
+  });
+
+  return std::all_of(POWER_OF_TWO.begin(), POWER_OF_TWO.end(), [](bool val) { return val; });
+}
+}  // namespace internal
+
+template <typename EnumT>
+concept UnsignedEnum =
+    requires { std::is_enum_v<EnumT>&& std::is_unsigned_v<typename std::underlying_type_t<EnumT>>; };
+
+/// This class allows to use enum classes as bit flags.
+/// Enum classes need to satisfy three properties:
+/// - Underline type is unsigned (checked by template concept)
+/// - All values are power of 2 (checked by static assert)
+/// - No duplicated values (not checked, and impossible to until we get reflection)
+/// Usage:
+/// enum class Flag : uint8_t { A = 1u << 1u, B = 1u << 2u, C = 1u << 3u };
+/// BitFlag<Flag> flag{ Flag::A };
+/// flag.set(Flag::B).set(Flag::C);
+/// flag.has(Flag::B); // true
+/// flag.unset(Flag::A);
+/// Variable containing multiple flags can be created as constexpr:
+/// constexpr auto D = BitFlag<Flag>{ Flag::B }.set(Flag::C); // == Flag::B | Flag::C
+/// flag.hasAnyOf(D); // true
+template <UnsignedEnum EnumT>
+class BitFlag {
+public:
+  // see https://dietertack.medium.com/using-bit-flags-in-c-d39ec6e30f08
+  using T = std::underlying_type_t<EnumT>;
+
+  constexpr BitFlag() : value_{ 0 } {
+  }
+
+  // NOLINTNEXTLINE(google-explicit-constructor, hicpp-explicit-conversions)
+  constexpr BitFlag(EnumT value) : value_{ static_cast<T>(value) } {
+    // NOTE: the constructor is not marked explicit to allow implicit conversion from EnumT to it.
+    // This allows to use the member methods with EnumT values directly at the cost of allocating the BitFlag
+    // value.
+    // TODO: consider if there is a way to avoid this allocation.
+    static_assert(internal::checkEnumValuesArePowerOf2<EnumT>(),
+                  "Enum is not valid for BitFlag, its values must be power of 2.");
+  }
+
+  constexpr auto reset() -> BitFlag& {
+    value_ = 0;
+    return *this;
+  }
+
+  /// Set the input flag(s)
+  constexpr auto set(BitFlag flag) -> BitFlag& {
+    value_ = value_ | flag.value_;
+    return *this;
+  }
+
+  /// Unset the given flag(s)
+  constexpr auto unset(BitFlag flag) -> BitFlag& {
+    value_ = value_ & static_cast<T>(~flag.value_);
+    return *this;
+  }
+
+  /// Returns true if the input flag is set.
+  /// In case of multiple input flags, returns true if all input flags are set.
+  /// `has` logically behaves as `hasAll`.
+  [[nodiscard]] constexpr auto has(BitFlag flag) const -> bool {
+    return (value_ & flag.value_) == flag.value_;
+  }
+
+  /// Returns true if any of the input flags are set.
+  /// In case of a single input flag, hasAny is equivalent to has.
+  [[nodiscard]] constexpr auto hasExactly(BitFlag flag) const -> bool {
+    return (value_ & flag.value_) == value_;
+  }
+
+  /// Returns true if any of the input flags are set
+  [[nodiscard]] constexpr auto hasAnyOf(BitFlag flag) const -> bool {
+    return (value_ & flag.value_) != 0u;
+  }
+
+private:
+  T value_;  // We need to store the underlying value to be able to store multiple flags which
+             // do not reprenset any enum value.
+};
+
+}  // namespace heph::utils
