b3543b0933d3a41107a0eab54ec586a023a4dc66
Filippo Brizzi <filippobrizzi@gmail.com>
2024-06-25 10:49:12 +0000

    install IWYU tool in dockerfile

--- docker/version.sh
+++ docker/version.sh
@@ -1,6 +1,6 @@
 HOST=ghcr.io/olympus-robotics
-VERSION=1.0.7
+VERSION=1.0.8
 IMAGE=hephaestus-dev
 
 # This is the version of the dep image. Increase this number everytime you change `external/CMakeLists.txt`
-DEPS_VERSION=1.0.14
+DEPS_VERSION=1.0.15
