f24f3aa8eed90ee4aac15b22deb54a7952e4aa86
Filippo Brizzi <filippobrizzi@gmail.com>
2024-07-01 14:57:49 +0200

    [Fix] TerminationBlocker stopRequested was not registering signal handler function (#109)
    # Description
    `TerminationBlocker::stopRequested` was not registering signal handler
    function.
    
    ## Type of change
    - Bug fix (non-breaking change which fixes an issue)

--- modules/utils/src/signal_handler.cpp
+++ modules/utils/src/signal_handler.cpp
@@ -5,10 +5,17 @@
 #include "hephaestus/utils/signal_handler.h"
 
 #include <csignal>
+#include <mutex>
 
 namespace heph::utils {
 
 auto TerminationBlocker::stopRequested() -> bool {
+  static std::once_flag flag;
+  std::call_once(flag, []() {
+    (void)signal(SIGINT, TerminationBlocker::signalHandler);
+    (void)signal(SIGTERM, TerminationBlocker::signalHandler);
+  });
+
   return instance().stop_flag_.test();
 }
 
