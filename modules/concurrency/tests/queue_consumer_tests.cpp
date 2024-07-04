a27d3ef41c4f3a8ca9840ea4d10ec0a9992bd365
Filippo Brizzi <filippobrizzi@gmail.com>
2024-07-04 13:01:56 +0000

    add missing file

---
+++ modules/concurrency/tests/queue_consumer_tests.cpp
@@ -0,0 +1,60 @@
+//=================================================================================================
+// Copyright (C) 2023-2024 HEPHAESTUS Contributors
+//=================================================================================================
+
+#include <algorithm>
+#include <atomic>
+#include <cstddef>
+#include <vector>
+
+#include <gtest/gtest.h>
+
+#include "hephaestus/concurrency/queue_consumer.h"
+#include "hephaestus/random/random_generator.h"
+#include "hephaestus/random/random_type.h"
+#include "hephaestus/utils/exception.h"
+
+// NOLINTNEXTLINE(google-build-using-namespace)
+using namespace ::testing;
+
+namespace heph::concurrency::tests {
+
+struct Message {
+  [[nodiscard]] auto operator==(const Message&) const -> bool = default;
+  int value;
+};
+
+TEST(QueueConsumer, Fail) {
+#ifndef DISABLE_EXCEPTION
+  EXPECT_THROW(QueueConsumer<Message>([](const Message&) {}, 0), InvalidParameterException);
+#endif
+}
+
+TEST(QueueConsumer, ProcessMessages) {
+  static constexpr std::size_t MESSAGE_COUNT = 2;
+  std::atomic_flag flag = ATOMIC_FLAG_INIT;
+  std::vector<Message> processed_messages;
+
+  QueueConsumer<Message> spinner{ [&processed_messages, &flag](const Message& message) {
+                                   processed_messages.push_back(message);
+                                   if (processed_messages.size() == MESSAGE_COUNT) {
+                                     flag.test_and_set();
+                                     flag.notify_all();
+                                   }
+                                 },
+                                  MESSAGE_COUNT };
+
+  auto mt = random::createRNG();
+  std::vector<Message> messages(MESSAGE_COUNT);
+  std::for_each(messages.begin(), messages.end(),
+                [&mt](Message& message) { message.value = random::randomT<int>(mt); });
+  for (const auto& message : messages) {
+    spinner.queue().forcePush(message);
+  }
+
+  flag.wait(false);
+
+  EXPECT_EQ(messages, processed_messages);
+}
+
+}  // namespace heph::concurrency::tests
