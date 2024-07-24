5006be69067c08cff8ec2ec0456debd0f5d5f871
Filippo Brizzi <filippobrizzi@gmail.com>
2024-07-24 08:36:32 +0000

    addressed comments

--- modules/telemetry/include/hephaestus/telemetry/measure_sink.h
+++ modules/telemetry/include/hephaestus/telemetry/measure_sink.h
@@ -13,8 +13,9 @@ using ClockT = std::chrono::system_clock;
 struct MeasureEntry {
   [[nodiscard]] auto operator==(const MeasureEntry& other) const -> bool = default;
 
-  std::string component;
-  std::string tag;
+  std::string component;  ///< The component that is logging the measure, e.g. SLAM, Navigation, etc.
+  std::string tag;        ///< The tag of the measure used to identify who created it, e.g. "front_camera",
+                          ///< "motor1", etc.
   ClockT::time_point measure_timestamp;
   std::string json_values;
 };
@@ -23,7 +24,7 @@ class IMeasureSink {
 public:
   virtual ~IMeasureSink() = default;
 
-  virtual void send(const MeasureEntry& measure_entry) = 0;
+  virtual void send(const MeasureEntry&) = 0;
 };
 
 }  // namespace heph::telemetry
