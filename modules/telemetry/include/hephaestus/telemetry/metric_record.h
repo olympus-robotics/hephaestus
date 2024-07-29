0a676e9cc6cf5ef3f39707b71b9a7fe693fbb25a
Filippo Brizzi <filippobrizzi@gmail.com>
2024-07-29 08:42:36 +0000

    fix comments

--- modules/telemetry/include/hephaestus/telemetry/metric_record.h
+++ modules/telemetry/include/hephaestus/telemetry/metric_record.h
@@ -14,25 +14,25 @@ namespace internal {
 jsonToValuesMap(std::string_view json) -> std::unordered_map<std::string, Metric::ValueType>;
 }
 
-/// @brief Register a new data point sink.
-/// For every data point recorded, the sink will be called to consume the data.
+/// @brief Register a new metric sink.
+/// For every metric recorded, the sink will be called to consume the data.
 /// There is no limit on the number of sinks supported.
 void registerMetricSink(std::unique_ptr<IMetricSink> sink);
 
-/// @brief Record a data point.
-/// The data point is forwarded to all registered sinks.
-/// Sinks process the data point in a dedicated thread, this means that this function is non-blocking and
+/// @brief Record a metric.
+/// The metric is forwarded to all registered sinks.
+/// Sinks process the metric in a dedicated thread, this means that this function is non-blocking and
 /// deterministic.
 void record(const Metric& metric);
 
-/// @brief Record a user defined data point.
+/// @brief Record a user defined metric.
 /// NOTE: the data needs to be serializable to JSON.
 /// For details on how to achieve this, see `heph::serdes::serializeToJSON`.
 /// @param component The component that is logging the metric_record, e.g. SLAM, Navigation, etc.
 /// @param tag The tag of the metric_record used to identify who created it, e.g. "front_camera",
 /// "motor1", etc.
 /// @param data The data to record, needs to be json serializable.
-/// @param timestamp The timestamp of the data point, if not provided, the current time is used.
+/// @param timestamp The timestamp of the metric, if not provided, the current time is used.
 template <typename DataT>
 void record(const std::string& component, const std::string& tag, std::size_t id, const DataT& data,
             ClockT::time_point timestamp = ClockT::now()) {
