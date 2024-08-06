1183ba6c88c4d92443ac0255ef4d565cfba87842
Filippo Brizzi <filippobrizzi@gmail.com>
2024-08-06 09:55:45 +0000

    enum upper case

--- modules/examples/examples/zenoh_service_server.cpp
+++ modules/examples/examples/zenoh_service_server.cpp
@@ -24,7 +24,7 @@ auto main(int argc, const char* argv[]) -> int {
   const heph::utils::StackTrace stack_trace;
 
   try {
-    auto desc = getProgramDescription("Binary service server example", ExampleType::Service);
+    auto desc = getProgramDescription("Binary service server example", ExampleType::SERVICE);
     const auto args = std::move(desc).parse(argc, argv);
 
     auto [session_config, topic_config] = parseArgs(args);
