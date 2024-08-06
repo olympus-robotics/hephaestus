5eae0631bd0ed1f9a6605758832e56c56a97f779
Filippo Brizzi <filippobrizzi@gmail.com>
2024-08-06 15:25:30 +0200

    change Enum to use UPPER_CASE (#141)
    # Description
    Enum values are constants and as such makes sense to threat them as
    constexpr.

--- modules/examples/examples/zenoh_program_options.h
+++ modules/examples/examples/zenoh_program_options.h
@@ -6,16 +6,27 @@
 #include "hephaestus/ipc/common.h"
 #include "hephaestus/utils/exception.h"
 
-enum class ExampleType : uint8_t { Pubsub, Service };
+enum class ExampleType : uint8_t { PUBSUB, SERVICE, ACTION_SERVER };
 
 [[nodiscard]] inline auto getProgramDescription(const std::string& description,
                                                 const ExampleType type) -> heph::cli::ProgramDescription {
   static constexpr auto DEFAULT_PUBSUB_KEY = "hephaestus/ipc/example/zenoh/put";
   static constexpr auto DEFAULT_SERVICE_KEY = "hephaestus/ipc/example/zenoh/service";
+  static constexpr auto DEFAULT_ACTION_SERVER_KEY = "hephaestus/ipc/example/zenoh/action_server";
+
+  const auto* const default_topic = [&type] {
+    switch (type) {
+      case ExampleType::PUBSUB:
+        return DEFAULT_PUBSUB_KEY;
+      case ExampleType::SERVICE:
+        return DEFAULT_SERVICE_KEY;
+      case ExampleType::ACTION_SERVER:
+        return DEFAULT_ACTION_SERVER_KEY;
+    }
+  }();
 
   auto desc = heph::cli::ProgramDescription(description);
-  desc.defineOption<std::string>("topic", 't', "Key expression",
-                                 type == ExampleType::Pubsub ? DEFAULT_PUBSUB_KEY : DEFAULT_SERVICE_KEY)
+  desc.defineOption<std::string>("topic", 't', "Key expression", default_topic)
       .defineOption<std::size_t>("cache", 'c', "Cache size", 0)
       .defineOption<std::string>("mode", 'm', "Running mode: options: peer, client", "peer")
       .defineOption<std::string>("router", 'r', "Router endpoint", "")
