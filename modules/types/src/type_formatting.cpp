7f14ef3fba34a4d5c97277b8690a2a3d2608b903
Lorenz Hruby <hruby@filics.eu>
2024-08-08 12:31:19 +0000

    Fix merge.

--- modules/types/src/type_formatting.cpp
+++ modules/types/src/type_formatting.cpp
@@ -2,8 +2,4 @@
 // Copyright (C) 2023-2024 HEPHAESTUS Contributors
 //=================================================================================================
 
-<<<<<<<< HEAD:modules/types/src/type_formatting.cpp
 #include "hephaestus/types/type_formatting.h"
-========
-#include "hephaestus/random/random_object_creator.h"  // NOLINT(misc-include-cleaner)
->>>>>>>> origin/main:modules/random/src/random_object_creator.cpp
