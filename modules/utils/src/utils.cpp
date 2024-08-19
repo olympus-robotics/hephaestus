a899b8efa192e0f23a20fb5a9458230b94df4243
Filippo Brizzi <filippobrizzi@gmail.com>
2024-08-19 15:52:17 +0000

    fix gethostname

--- modules/utils/src/utils.cpp
+++ modules/utils/src/utils.cpp
@@ -2,6 +2,20 @@
 // Copyright (C) 2023-2024 HEPHAESTUS Contributors
 //=================================================================================================
 
-#include "hephaestus/utils/utils.h"  // NOLINT(misc-include-cleaner)
+#include "hephaestus/utils/utils.h"
 
-namespace heph::utils {}  // namespace heph::utils
+#include <string>
+
+#include <sys/param.h>  // NOLINT(misc-include-cleaner)
+#include <unistd.h>
+
+namespace heph::utils {
+auto getHostName() -> std::string {
+  char host_name[MAXHOSTNAMELEN + 1] = { '\0' };  // NOLINT(cppcoreguidelines-avoid-c-arrays,
+                                                  // misc-include-cleaner)
+  // NOLINTNEXTLINE(cppcoreguidelines-pro-bounds-array-to-pointer-decay)
+  const auto ret = ::gethostname(host_name, sizeof(host_name));
+  // NOLINTNEXTLINE(cppcoreguidelines-pro-bounds-array-to-pointer-decay)
+  return ((ret == 0) ? (std::string(host_name)) : (""));
+}
+}  // namespace heph::utils
