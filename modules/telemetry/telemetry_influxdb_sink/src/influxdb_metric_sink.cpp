2c57aaee7abf1cf46170e6d5cd4540f3e67d996f
Filippo Brizzi <filippobrizzi@gmail.com>
2024-08-19 09:23:12 +0000

    telemetry add terminal sink; manage nan

--- modules/telemetry/telemetry_influxdb_sink/src/influxdb_metric_sink.cpp
+++ modules/telemetry/telemetry_influxdb_sink/src/influxdb_metric_sink.cpp
@@ -5,10 +5,12 @@
 #include "hephaestus/telemetry_influxdb_sink/influxdb_metric_sink.h"
 
 #include <cerrno>
+#include <cstdint>
 #include <cstdlib>
 #include <exception>
 #include <memory>
 #include <string>
+#include <type_traits>
 #include <utility>
 #include <variant>
 
@@ -19,9 +21,21 @@
 #include <fmt/core.h>
 
 #include "hephaestus/telemetry/metric_sink.h"
+#include "hephaestus/utils/string/string_utils.h"
 
 namespace heph::telemetry_sink {
 namespace {
+
+[[nodiscard]] auto isNaN(const auto& value) -> bool {
+  if constexpr (std::is_same_v<std::decay_t<decltype(value)>, double> ||
+                std::is_same_v<std::decay_t<decltype(value)>, int64_t>) {
+    return std::isnan(value);
+  } else if constexpr (std::is_same_v<std::decay_t<decltype(value)>, std::string>) {
+    return utils::string::toLowerCase(value) == "nan";
+  }
+
+  return false;
+}
 
 [[nodiscard]] auto createInfluxdbPoint(const telemetry::Metric& entry) -> influxdb::Point {
   auto point = influxdb::Point{ entry.component }
@@ -29,7 +43,22 @@ namespace {
                    .addTag("id", std::to_string(entry.id))
                    .setTimestamp(entry.timestamp);
   for (const auto& [key, value] : entry.values) {
-    std::visit([&point, &key](auto&& arg) { point.addField(key, arg); }, value);
+    std::visit(
+        [&point, &key](auto&& arg) {
+          if (isNaN(arg)) {
+            return;
+          }
+
+          // When coming from JSON a value can be erronously first creted as int and then become double.
+          // Influxdb forbid mixing types in the same field, so we need to convert every int to double.
+          if constexpr (std::is_same_v<std::decay_t<decltype(arg)>, int64_t>) {
+            point.addField(key, static_cast<double>(arg));
+            return;
+          }
+
+          point.addField(key, arg);
+        },
+        value);
   }
 
   return point;
