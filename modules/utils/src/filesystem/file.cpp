9e91e3fd09272a9a349204087b0cef58f273c195
Filippo Brizzi <filippobrizzi@gmail.com>
2024-08-19 10:12:05 +0000

    add file utils

--- modules/utils/src/filesystem/file.cpp
+++ modules/utils/src/filesystem/file.cpp
@@ -5,20 +5,23 @@
 #include <fstream>
 #include <ios>
 #include <iterator>
+#include <optional>
 #include <span>
 #include <string>
 #include <string_view>
 #include <vector>
 
-#include <fmt/core.h>
-
-#include "hephaestus/utils/exception.h"
+#ifdef __linux__
+#include <linux/limits.h>
+#endif
+#include <unistd.h>
 
 namespace heph::utils::filesystem {
-auto readFile(const std::filesystem::path& path) -> std::string {
+auto readFile(const std::filesystem::path& path) -> std::optional<std::string> {
   std::ifstream infile{ path };
-  throwExceptionIf<InvalidDataException>(!infile,
-                                         fmt::format("Could not open file {} to read", path.string()));
+  if (!infile) {
+    return {};
+  }
 
   std::string text;
   text.reserve(std::filesystem::file_size(path));
@@ -27,10 +30,11 @@ auto readFile(const std::filesystem::path& path) -> std::string {
   return text;
 }
 
-auto readBinaryFile(const std::filesystem::path& path) -> std::vector<std::byte> {
+auto readBinaryFile(const std::filesystem::path& path) -> std::optional<std::vector<std::byte>> {
   std::ifstream infile{ path, std::ios::binary };
-  throwExceptionIf<InvalidDataException>(!infile,
-                                         fmt::format("Could not open file {} to read", path.string()));
+  if (!infile) {
+    return {};
+  }
 
   const auto file_size = std::filesystem::file_size(path);
   std::vector<std::byte> buffer(file_size);
@@ -40,21 +44,54 @@ auto readBinaryFile(const std::filesystem::path& path) -> std::vector<std::byte>
   return buffer;
 }
 
-void writeStringToFile(const std::filesystem::path& path, std::string_view content) {
+auto writeStringToFile(const std::filesystem::path& path, std::string_view content) -> bool {
   std::ofstream outfile{ path, std::ios::out };
-  throwExceptionIf<InvalidDataException>(!outfile,
-                                         fmt::format("Could not open file {} to write", path.string()));
+  if (!outfile) {
+    return false;
+  }
 
   outfile.write(content.data(), static_cast<std::streamsize>(content.size()));
+  return true;
 }
 
-void writeBufferToFile(const std::filesystem::path& path, std::span<const std::byte> content) {
+auto writeBufferToFile(const std::filesystem::path& path, std::span<const std::byte> content) -> bool {
   std::ofstream outfile{ path, std::ios::out | std::ios::binary };
-  throwExceptionIf<InvalidDataException>(!outfile,
-                                         fmt::format("Could not open file {} to write", path.string()));
+  if (!outfile) {
+    return false;
+  }
 
   // NOLINTNEXTLINE(cppcoreguidelines-pro-type-reinterpret-cast)
   outfile.write(reinterpret_cast<const char*>(content.data()), static_cast<std::streamsize>(content.size()));
+  return true;
+}
+
+auto searchFilenameInPaths(const std::string& filename, const std::vector<std::filesystem::path>& paths)
+    -> std::optional<std::filesystem::path> {
+  if (std::filesystem::exists(filename)) {
+    return filename;
+  }
+
+  for (const auto& path : paths) {
+    auto file = path / filename;
+    if (std::filesystem::exists(file)) {
+      return file;
+    }
+  }
+
+  return {};
+}
+
+auto getThisExecutableFullPath() -> std::filesystem::path {
+/// \note This implementation only works for Linux
+#ifdef __linux__
+  // NOLINTBEGIN(cppcoreguidelines-pro-bounds-array-to-pointer-decay)
+  char path[PATH_MAX + 1] = { '\0' };  // NOLINT(cppcoreguidelines-avoid-c-arrays)
+  const auto path_length = readlink("/proc/self/exe", path, PATH_MAX);
+  return ((path_length > 0) ? std::filesystem::path{ path } : (""));
+#elif
+  return {};
+#endif
+  // NOLINTEND(cppcoreguidelines-pro-bounds-array-to-pointer-decay)
 }
 
 }  // namespace heph::utils::filesystem
