9272bce3526731aee4eafaeed73b01fef7182d4f
Lorenz Hruby <hruby@filics.eu>
2024-09-24 18:35:25 +0200

    Feat/Extend type formatting (#171)
    # Description
    * Add enum, std::array and std::unordered_map to type formatting
    * Extend concepts
    
    ## Type of change
    - New feature (non-breaking change which adds functionality)
    - Breaking change (fix or feature that would cause existing
    functionality to not work as expected)
    
    ## Checklist before requesting a review
    - [x] I have performed a self-review of my code.
    - [x] If it is a core feature, I have added thorough tests.
    - [ ] If this is a new component I have added examples.
    - [ ] I updated the README and related documentation.

--- modules/utils/include/hephaestus/utils/concepts.h
+++ modules/utils/include/hephaestus/utils/concepts.h
@@ -11,7 +11,39 @@
 namespace heph {
 
 template <typename T>
-concept ScalarType = requires(T a) { std::is_scalar_v<T>; };
+concept ScalarType = std::is_scalar_v<T>;
+
+template <typename T>
+concept EnumType = std::is_enum_v<T>;
+
+template <typename T>
+concept ArrayType = requires {
+  typename T::value_type;
+  requires std::same_as<T, std::array<typename T::value_type, T().size()>>;
+};
+
+template <typename T>
+concept VectorType = requires {
+  typename T::value_type;
+  typename T::allocator_type;
+  requires std::same_as<T, std::vector<typename T::value_type, typename T::allocator_type>>;
+};
+
+template <typename T>
+concept UnorderedMapType = requires(T t) {
+  typename T::key_type;
+  typename T::mapped_type;
+  typename T::hasher;
+  typename T::key_equal;
+  std::is_same_v<T, std::unordered_map<typename T::key_type, typename T::mapped_type, typename T::hasher,
+                                       typename T::key_equal>>;
+};
+
+template <typename T>
+concept ChronoSystemClockType = std::is_same_v<T, std::chrono::system_clock>;
+
+template <typename T>
+concept ChronoSteadyClockType = std::is_same_v<T, std::chrono::steady_clock>;
 
 /// Types that are convertable to and from a string
 template <typename T>
