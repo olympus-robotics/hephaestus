c51ca050231e6a7d3bc0db439cfb0b17ab2acfa3
Lorenz Hruby <hruby@filics.eu>
2024-09-24 11:31:35 +0000

    Bump version.

--- toolchains/toolchain_clang.cmake
+++ toolchains/toolchain_clang.cmake
@@ -1,6 +1,7 @@
 # =================================================================================================
 # Copyright (C) 2023-2024 HEPHAESTUS Contributors MIT License
 # =================================================================================================
+
 if(${CMAKE_SYSTEM_NAME} MATCHES "Darwin")
   set(LLVM_PATH /opt/homebrew/Cellar/llvm/17.0.6_1/bin)
   set(CMAKE_C_COMPILER ${LLVM_PATH}/clang)
@@ -10,6 +11,5 @@ else()
   set(CMAKE_CXX_COMPILER clang++)
 endif()
 set(CMAKE_CXX_FLAGS "-stdlib=libc++")
-set(CMAKE_EXE_LINKER_FLAGS "-stdlib=libc++ -lc++abi -fuse-ld=lld")
 set(CMAKE_EXE_LINKER_FLAGS "-stdlib=libc++ -lc++abi -fuse-ld=lld")
 set(CMAKE_CROSSCOMPILING FALSE)
