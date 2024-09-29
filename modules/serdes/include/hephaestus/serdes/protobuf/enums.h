58a1afd48784f3b2934ee0130de6f7f6b2ef7417
Lorenz Hruby <hruby@filics.eu>
2024-09-29 15:56:03 +0000

    Make tests robust.

--- modules/serdes/include/hephaestus/serdes/protobuf/enums.h
+++ modules/serdes/include/hephaestus/serdes/protobuf/enums.h
@@ -98,9 +98,9 @@ template <EnumType ProtoT, EnumType T>
 auto toProtoEnum(const T& enum_value) -> ProtoT {
   static const auto enum_to_proto_enum = internal::createEnumLookupTable<ProtoT, T>();
   const auto it = enum_to_proto_enum.find(enum_value);
-  throwExceptionIf<InvalidDataException>(it == enum_to_proto_enum.end(),
-                                         "Enum {} not found in the lookup table",
-                                         utils::format::toString(enum_value));
+  throwExceptionIf<InvalidDataException>(
+      it == enum_to_proto_enum.end(),
+      fmt::format("Enum {} not found in the lookup table", utils::format::toString(enum_value)));
   return it->second;
 }
 
@@ -109,9 +109,9 @@ auto fromProto(const ProtoT& proto_enum_value, T& enum_value) -> void {
   static const auto proto_enum_value_to_enum =
       internal::createInverseLookupTable(internal::createEnumLookupTable<ProtoT, T>());
   const auto it = proto_enum_value_to_enum.find(proto_enum_value);
-  throwExceptionIf<InvalidDataException>(it == proto_enum_value_to_enum.end(),
-                                         "Enum {} not found in the lookup table",
-                                         utils::format::toString(proto_enum_value));
+  throwExceptionIf<InvalidDataException>(
+      it == proto_enum_value_to_enum.end(),
+      fmt::format("Enum {} not found in the lookup table", utils::format::toString(proto_enum_value)));
   enum_value = it->second;
   ;
 }
