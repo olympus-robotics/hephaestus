bc1ff5b8b6577366bffbfc3fff08a7906a204441
Lorenz Hruby <hruby@filics.eu>
2024-09-30 11:55:19 +0200

    Feat/Types proto enum serialization (#176)
    # Description
    Automate serialization of enums using protobuf.
    
    ## Type of change
    - New feature (non-breaking change which adds functionality)
    
    ## Checklist before requesting a review
    - [x] I have performed a self-review of my code.
    - [x] If it is a core feature, I have added thorough tests.
    - [x] If this is a new component I have added examples.
    - [x] I updated the README and related documentation.
    
    ---------
    
    Co-authored-by: Filippo Brizzi <filippobrizzi@gmail.com>

---
+++ modules/serdes/include/hephaestus/serdes/protobuf/enums.h
@@ -0,0 +1,117 @@
+//=================================================================================================
+// Copyright (C) 2023-2024 HEPHAESTUS Contributors
+//=================================================================================================
+
+#pragma once
+
+#include <string_view>
+#include <type_traits>
+#include <unordered_map>
+
+#include <fmt/core.h>
+#include <magic_enum.hpp>
+
+#include "hephaestus/utils/concepts.h"
+#include "hephaestus/utils/exception.h"
+#include "hephaestus/utils/format/format.h"
+#include "hephaestus/utils/string/string_literal.h"
+#include "hephaestus/utils/string/string_utils.h"
+
+namespace heph::serdes::protobuf {
+
+template <EnumType ProtoT, EnumType T>
+[[nodiscard]] auto toProtoEnum(const T& enum_value) -> ProtoT;
+
+template <EnumType ProtoT, EnumType T>
+auto fromProto(const ProtoT& proto_enum_value, T& enum_value) -> void;
+
+//=================================================================================================
+// Implementation
+//=================================================================================================
+
+namespace internal {
+template <EnumType ProtoT>
+[[nodiscard]] auto getProtoPrefix() -> std::string {
+  const auto enum_type_name = magic_enum::enum_type_name<ProtoT>();
+
+  // Underscores indicate nested proto enums, no underscore indicates a top-level proto enum.
+  if (const auto underscore_pos = std::find(enum_type_name.begin(), enum_type_name.end(), '_');
+      underscore_pos == enum_type_name.end()) {
+    // Top-level enums use the enum name as a prefix: ENUM_NAME_ENUM_VALUE.
+    return utils::string::toScreamingSnakeCase(enum_type_name);
+  }
+
+  // Nested enums have a prefix, and enum values are separated by an underscore:
+  // ClassName_EnumName_ENUM_VALUE.
+  return fmt::format("{}", enum_type_name);
+}
+
+/// Convert between enums and their protobuf counterparts. The following convention is used:
+/// enum class FooExternalEnum : { BAR1, BAR2 };
+/// struct Foo {
+///   enum class InternalEnum : { BAR1, BAR2 };
+/// };
+/// will have a protobuf counterpart
+/// enum FooExternalEnum : { FOO_EXTERNAL_ENUM_BAR1,  FOO_EXTERNAL_ENUM_BAR2 };
+/// enum Foo_InternalEnum : { Foo_InternalEnum_BAR1, Foo_InternalEnum_BAR2 };
+template <EnumType ProtoT, EnumType T>
+[[nodiscard]] auto getAsProtoEnum(T e) -> ProtoT {
+  const auto proto_enum_name = fmt::format("{}_{}", getProtoPrefix<ProtoT>(), magic_enum::enum_name(e));
+  const auto proto_enum = magic_enum::enum_cast<ProtoT>(proto_enum_name);
+
+  heph::throwExceptionIf<heph::InvalidParameterException>(
+      !proto_enum.has_value(),
+      fmt::format("The proto enum does not contain the requested key {}. Proto enum keys are\n{}",
+                  proto_enum_name, utils::format::toString(magic_enum::enum_names<ProtoT>())));
+
+  return proto_enum.value();  // NOLINT(bugprone-unchecked-optional-access)
+}
+
+template <EnumType ProtoT, EnumType T>
+[[nodiscard]] auto createEnumLookupTable() -> std::unordered_map<T, ProtoT> {
+  std::unordered_map<T, ProtoT> lookup_table;
+
+  for (const auto& e : magic_enum::enum_values<T>()) {
+    lookup_table[e] = getAsProtoEnum<ProtoT, T>(e);
+  }
+
+  return lookup_table;
+}
+
+/// @brief Create inverse lookup table. Unique values are guaranteed by the enum layout.
+template <EnumType ProtoT, EnumType T>
+[[nodiscard]] auto createInverseLookupTable(const std::unordered_map<T, ProtoT>& enum_to_proto_enum)
+    -> std::unordered_map<ProtoT, T> {
+  std::unordered_map<ProtoT, T> proto_enum_to_enum;
+
+  for (const auto& kvp : enum_to_proto_enum) {
+    proto_enum_to_enum.insert({ kvp.second, kvp.first });
+  }
+
+  return proto_enum_to_enum;
+}
+}  // namespace internal
+
+template <EnumType ProtoT, EnumType T>
+auto toProtoEnum(const T& enum_value) -> ProtoT {
+  static const auto enum_to_proto_enum = internal::createEnumLookupTable<ProtoT, T>();
+  const auto it = enum_to_proto_enum.find(enum_value);
+  throwExceptionIf<InvalidDataException>(
+      it == enum_to_proto_enum.end(),
+      fmt::format("Enum {} not found in the lookup table", utils::format::toString(enum_value)));
+  return it->second;
+}
+
+template <EnumType ProtoT, EnumType T>
+auto fromProto(const ProtoT& proto_enum_value, T& enum_value) -> void {
+  static const auto proto_enum_value_to_enum =
+      internal::createInverseLookupTable(internal::createEnumLookupTable<ProtoT, T>());
+  const auto it = proto_enum_value_to_enum.find(proto_enum_value);
+  throwExceptionIf<InvalidDataException>(
+      it == proto_enum_value_to_enum.end(),
+      fmt::format("Enum {} not found in the lookup table", utils::format::toString(proto_enum_value)));
+  enum_value = it->second;
+  ;
+}
+
+}  // namespace heph::serdes::protobuf
--- modules/types/src/dummy_type.cpp
+++ modules/types/src/dummy_type.cpp
@@ -8,8 +8,6 @@
 #include <iostream>
 #include <random>
 
-#include <magic_enum.hpp>
-
 #include "hephaestus/random/random_object_creator.h"
 #include "hephaestus/utils/format/format.h"
 
@@ -48,7 +46,8 @@ auto operator<<(std::ostream& os, const DummyPrimitivesType& dummy_primitives_ty
 
 auto DummyType::random(std::mt19937_64& mt) -> DummyType {
   return { .dummy_primitives_type = random::random<decltype(dummy_primitives_type)>(mt),
-           .dummy_enum = random::random<decltype(dummy_enum)>(mt),
+           .internal_dummy_enum = random::random<decltype(internal_dummy_enum)>(mt),
+           .external_dummy_enum = random::random<decltype(external_dummy_enum)>(mt),
            .dummy_string = random::random<decltype(dummy_string)>(mt),
            .dummy_vector = random::random<decltype(dummy_vector)>(mt) };
 }
@@ -56,7 +55,8 @@ auto DummyType::random(std::mt19937_64& mt) -> DummyType {
 auto operator<<(std::ostream& os, const DummyType& dummy_type) -> std::ostream& {
   return os << "DummyType{\n"
             << "  dummy_primitives_type={" << dummy_type.dummy_primitives_type << "}\n"
-            << "  dummy_enum=" << magic_enum::enum_name(dummy_type.dummy_enum) << "\n"
+            << "  internal_dummy_enum=" << utils::format::toString(dummy_type.internal_dummy_enum) << "\n"
+            << "  external_dummy_enum=" << utils::format::toString(dummy_type.external_dummy_enum) << "\n"
             << "  dummy_string=" << dummy_type.dummy_string << "\n"
             << "  dummy_vector=" << utils::format::toString(dummy_type.dummy_vector) << "\n"
             << "}";
