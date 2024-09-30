254e8922599a0dbb44e0ffa49448998f48db2096
Filippo Brizzi <filippobrizzi@gmail.com>
2024-09-30 12:50:02 +0200

    [Fix] Command line arg parser was not properly parsing negative number values (#179)
    Negative numbers were treated as new options.

--- modules/cli/tests/program_options_tests.cpp
+++ modules/cli/tests/program_options_tests.cpp
@@ -28,10 +28,11 @@ TEST(ProgramOptions, Option) {
       .defineOption<std::string>("other_option", 'o', "other desc")
       .defineOption<float>("bar", "desc", NUMBER)
       .defineOption<int>("foo", 'f', "desc", 1)
+      .defineOption<float>("baz", "desc")
       .defineFlag("flag", 'b', "desc");
   {
-    auto options =
-        ProgramDescription{ desc }.parse({ "--option", "value", "-o", "other_value", "--bar", "1.2" });
+    auto options = ProgramDescription{ desc }.parse(
+        { "--option", "value", "-o", "other_value", "--bar", "1.2", "--baz", "-1.0" });
     EXPECT_TRUE(options.hasOption("option"));
     EXPECT_EQ(options.getOption<std::string>("option"), "value");
     EXPECT_TRUE(options.hasOption("other_option"));
@@ -40,12 +41,14 @@ TEST(ProgramOptions, Option) {
     EXPECT_EQ(options.getOption<float>("bar"), 1.2f);
     EXPECT_TRUE(options.hasOption("foo"));
     EXPECT_EQ(options.getOption<int>("foo"), 1);
+    EXPECT_TRUE(options.hasOption("baz"));
+    EXPECT_EQ(options.getOption<float>("baz"), -1.0);
     EXPECT_TRUE(options.hasOption("flag"));
     EXPECT_FALSE(options.getOption<bool>("flag"));
   }
   {
     auto options = ProgramDescription{ desc }.parse(
-        { "--option", "value", "-o", "other_value", "--bar", "1.2", "--flag" });
+        { "--option", "value", "-o", "other_value", "--bar", "1.2", "--baz", "-20", "--flag" });
     EXPECT_TRUE(options.hasOption("flag"));
     EXPECT_TRUE(options.getOption<bool>("flag"));
   }
@@ -68,6 +71,7 @@ TEST(ProgramOptions, Errors) {
     EXPECT_THROW(ProgramDescription{ desc }.parse({ "--option", "value", "other_value" }),
                  InvalidParameterException);
     EXPECT_THROW(ProgramDescription{ desc }.parse({ "--option", "value" }), InvalidConfigurationException);
+    EXPECT_THROW(ProgramDescription{ desc }.parse({ "--option", "-o" }), InvalidParameterException);
   }
 
   {
