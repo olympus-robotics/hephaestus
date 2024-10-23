5e715f7d6bafb08f9d8e6fc9edaea5c9e3dfe0ba
Filippo Brizzi <filippobrizzi@gmail.com>
2024-10-23 08:50:02 +0000

    fix

--- modules/ipc/src/zenoh/action_server/types_proto.cpp
+++ modules/ipc/src/zenoh/action_server/types_proto.cpp
@@ -7,61 +7,19 @@
 
 #include "hephaestus/ipc/zenoh/action_server/proto/types.pb.h"
 #include "hephaestus/ipc/zenoh/action_server/types.h"
+#include "hephaestus/serdes/protobuf/enums.h"
 
 namespace heph::ipc::zenoh::action_server {
 // TODO(@filippobrizzi): for some reason clang-tidy doesn't understand that this are public function declared
-// in the header. NOLINTBEGIN(misc-use-internal-linkage)
-void toProto(proto::RequestStatus& proto_status, const RequestStatus& status) {
-  switch (status) {
-    case RequestStatus::SUCCESSFUL:
-      proto_status = proto::REQUEST_STATUS_SUCCESSFUL;
-      break;
-    case RequestStatus::REJECTED_USER:
-      proto_status = proto::REQUEST_STATUS_REJECTED_USER;
-      break;
-    case RequestStatus::REJECTED_ALREADY_RUNNING:
-      proto_status = proto::REQUEST_STATUS_REJECTED_ALREADY_RUNNING;
-      break;
-    case RequestStatus::INVALID:
-      proto_status = proto::REQUEST_STATUS_INVALID;
-      break;
-    case RequestStatus::STOPPED:
-      proto_status = proto::REQUEST_STATUS_STOPPED;
-      break;
-  }
-}
-
-void fromProto(const proto::RequestStatus& proto_status, RequestStatus& status) {
-  switch (proto_status) {
-    case proto::REQUEST_STATUS_SUCCESSFUL:
-      status = RequestStatus::SUCCESSFUL;
-      break;
-    case proto::REQUEST_STATUS_REJECTED_USER:
-      status = RequestStatus::REJECTED_USER;
-      break;
-    case proto::REQUEST_STATUS_REJECTED_ALREADY_RUNNING:
-      status = RequestStatus::REJECTED_ALREADY_RUNNING;
-      break;
-    case proto::REQUEST_STATUS_INVALID:
-      status = RequestStatus::INVALID;
-      break;
-    case proto::REQUEST_STATUS_STOPPED:
-      status = RequestStatus::STOPPED;
-      break;
-    default:
-      LOG(ERROR) << "Unknown RequestStatus: " << magic_enum::enum_name(proto_status);
-      status = RequestStatus::REJECTED_USER;
-  }
-}
+// in the header.
+/// NOLINTBEGIN(misc-use-internal-linkage)
 
 void toProto(proto::RequestResponse& proto_response, const RequestResponse& response) {
-  proto::RequestStatus proto_status{};
-  toProto(proto_status, response.status);
-  proto_response.set_status(proto_status);
+  proto_response.set_status(serdes::protobuf::toProtoEnum<proto::RequestStatus>(response.status));
 }
 
 void fromProto(const proto::RequestResponse& proto_response, RequestResponse& response) {
-  fromProto(proto_response.status(), response.status);
+  serdes::protobuf::fromProto(proto_response.status(), response.status);
 }
 // NOLINTEND(misc-use-internal-linkage)
 }  // namespace heph::ipc::zenoh::action_server
