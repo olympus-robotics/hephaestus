a86803e322d6fd5acf0b53007d7cb6f0923dbdda
Filippo Brizzi <filippobrizzi@gmail.com>
2024-10-23 09:45:30 +0000

    fix

--- modules/ipc/src/zenoh/action_server/types_proto.cpp
+++ modules/ipc/src/zenoh/action_server/types_proto.cpp
@@ -2,9 +2,6 @@
 // Copyright (C) 2023-2024 HEPHAESTUS Contributors
 //=================================================================================================
 
-#include <absl/log/log.h>
-#include <magic_enum.hpp>
-
 #include "hephaestus/ipc/zenoh/action_server/proto/types.pb.h"
 #include "hephaestus/ipc/zenoh/action_server/types.h"
 #include "hephaestus/serdes/protobuf/enums.h"
@@ -12,8 +9,7 @@
 namespace heph::ipc::zenoh::action_server {
 // TODO(@filippobrizzi): for some reason clang-tidy doesn't understand that this are public function declared
 // in the header.
-/// NOLINTBEGIN(misc-use-internal-linkage)
-
+// NOLINTBEGIN(misc-use-internal-linkage)
 void toProto(proto::RequestResponse& proto_response, const RequestResponse& response) {
   proto_response.set_status(serdes::protobuf::toProtoEnum<proto::RequestStatus>(response.status));
 }
