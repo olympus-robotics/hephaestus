16d2c8c084a4c116c4a88bdf3773467e7f301f4e
Markus Weber <markus.weberino@gmail.com>
2024-11-19 10:35:33 +0000

    fix build

--- modules/ipc/src/zenoh/session.cpp
+++ modules/ipc/src/zenoh/session.cpp
@@ -40,7 +40,7 @@ auto createZenohConfig(const Config& config) -> ::zenoh::Config {
     zconfig.insert_json5("scouting/multicast/enabled", "true");  // NOLINT(misc-include-cleaner)
   }
   const auto multicast_scouting_interface = fmt::format(R"("{}")", config.multicast_scouting_interface);
-  zconfig.insert_json5(Z_CONFIG_MULTICAST_INTERFACE_KEY,
+  zconfig.insert_json5("scouting/multicast/interface",
                        multicast_scouting_interface);  // NOLINT(misc-include-cleaner)
 
   // Set the transport to UDP, but I am not sure it is the right way.
