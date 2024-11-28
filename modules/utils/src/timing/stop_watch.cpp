fd3b658867d5c3273f4574574052a873f5ad37f2
Filippo Brizzi <filippobrizzi@gmail.com>
2024-11-28 16:08:16 +0000

    Add StopWatch (#215)
    # Description
    Create a StopWatch

---
+++ modules/utils/src/timing/stop_watch.cpp
@@ -0,0 +1,81 @@
+//=================================================================================================
+// PIPPO © Copyright, 2015-2023. All Rights Reserved.
+//
+// This file is part of the RoboCool project
+//=================================================================================================
+
+#include "hephaestus/utils/timing/stop_watch.h"
+
+#include <chrono>
+#include <cstddef>
+#include <optional>
+
+namespace heph::utils::timing {
+
+StopWatch::StopWatch() {
+  reset();
+}
+
+void StopWatch::start() {
+  if (lap_start_timestamp_.has_value()) {
+    return;
+  }
+
+  lap_start_timestamp_ = ClockT::now();
+  if (!initial_start_timestamp_.has_value()) {
+    initial_start_timestamp_ = lap_start_timestamp_.value();
+  }
+}
+
+void StopWatch::reset() {
+  lap_start_timestamp_ = std::nullopt;
+  initial_start_timestamp_ = std::nullopt;
+  lapse_timestamp_ = std::nullopt;
+  accumulated_time_ = {};
+  lap_counter_ = 0;
+}
+
+auto StopWatch::accumulatedLapsDuration() const -> ClockT::duration {
+  return (lap_start_timestamp_.has_value() ?
+              (accumulated_time_ + (ClockT::now() - lap_start_timestamp_.value())) :
+              accumulated_time_);
+}
+
+auto StopWatch::initialStartTimestamp() const -> std::optional<ClockT::time_point> {
+  return initial_start_timestamp_;
+}
+
+auto StopWatch::lapsCount() const -> std::size_t {
+  return lap_counter_;
+}
+
+auto StopWatch::lapseImpl() -> ClockT::duration {
+  if (!lap_start_timestamp_.has_value()) {
+    return {};
+  }
+
+  auto lapse_start_timestamp =
+      lapse_timestamp_.has_value() ? lapse_timestamp_.value() : lap_start_timestamp_.value();
+
+  lapse_timestamp_ = ClockT::now();
+
+  return lapse_timestamp_.value() - lapse_start_timestamp;
+}
+
+auto StopWatch::stopImpl() -> ClockT::duration {
+  if (!lap_start_timestamp_.has_value()) {
+    return {};
+  }
+
+  auto stop_timestamp = ClockT::now();
+  const auto lap_time = stop_timestamp - lap_start_timestamp_.value();
+
+  lap_start_timestamp_ = std::nullopt;
+  lapse_timestamp_ = std::nullopt;
+  accumulated_time_ += lap_time;
+  ++lap_counter_;
+
+  return lap_time;
+}
+
+}  // namespace heph::utils::timing
---
+++ modules/utils/tests/stop_watch_tests.cpp
@@ -0,0 +1,115 @@
+//=================================================================================================
+// Copyright (C) 2023-2024 HEPHAESTUS Contributors
+//=================================================================================================
+
+#include <thread>
+
+#include <gtest/gtest.h>
+
+#include "hephaestus/utils/timing/stop_watch.h"
+
+// NOLINTNEXTLINE(google-build-using-namespace)
+using namespace ::testing;
+
+namespace heph::utils::timing::tests {
+TEST(StopWatch, AccumulateTime) {
+  using namespace std::chrono_literals;
+  constexpr auto PERIOD = 100ms;
+
+  StopWatch swatch;
+  swatch.start();
+  const auto t1 = swatch.accumulatedLapsDuration();
+  EXPECT_GT(t1.count(), 0);
+  std::this_thread::sleep_for(PERIOD);
+  const auto t2 = swatch.accumulatedLapsDuration();
+  EXPECT_LT(t1, t2);
+}
+
+TEST(StopWatch, Stoppable) {
+  using namespace std::chrono_literals;
+  static constexpr auto PERIOD = 10ms;
+
+  StopWatch swatch;
+  swatch.start();
+
+  std::this_thread::sleep_for(PERIOD);
+  const auto elapsed = swatch.stop();
+  EXPECT_EQ(swatch.getLapsCount(), 1);
+
+  const auto t1 = swatch.accumulatedLapsDuration();
+  EXPECT_EQ(elapsed, t1);
+  EXPECT_GT(t1.count(), 0);
+
+  std::this_thread::sleep_for(PERIOD);
+  const auto t2 = swatch.accumulatedLapsDuration();
+  EXPECT_EQ(t1.count(), t2.count());
+}
+
+TEST(StopWatch, ResumeCounting) {
+  using namespace std::chrono_literals;
+  constexpr auto PERIOD = 10ms;
+
+  StopWatch swatch;
+
+  // start and let it run for period
+  swatch.start();
+  std::this_thread::sleep_for(PERIOD);
+
+  // stop for a while
+  std::ignore = swatch.stop();
+  std::this_thread::sleep_for(PERIOD);
+
+  // start again and let it run for period
+  swatch.start();
+  std::this_thread::sleep_for(PERIOD);
+  // should have accumulated about 2 periods of count
+  const auto t2 = swatch.accumulatedLapsDuration();
+  EXPECT_GT(t2, 2 * PERIOD - 1ms);
+
+  std::ignore = swatch.stop();
+  EXPECT_EQ(swatch.lapsCount(), 2);
+}
+
+TEST(StopWatch, Reset) {
+  using namespace std::chrono_literals;
+  constexpr auto PERIOD = 10ms;
+
+  StopWatch swatch;
+  swatch.start();
+  std::this_thread::sleep_for(PERIOD);
+  std::ignore = swatch.stop();
+
+  EXPECT_NE(swatch.accumulatedLapsDuration().count(), 0);
+
+  swatch.reset();
+  EXPECT_EQ(swatch.accumulatedLapsDuration().count(), 0);
+  EXPECT_EQ(swatch.lapsCount(), 0);
+}
+
+TEST(StopWatch, Lapse) {
+  using namespace std::chrono_literals;
+  constexpr auto PERIOD = 10ms;
+
+  StopWatch swatch;
+  swatch.start();
+  std::this_thread::sleep_for(PERIOD);
+  const auto l1 = swatch.lapse();
+  const auto l2 = swatch.lapse();
+  const auto d = swatch.stop();
+  EXPECT_GT(l1, l2);
+  EXPECT_GT(d, l2);
+}
+
+TEST(StopWatch, DurationCast) {
+  using namespace std::chrono_literals;
+  using DurationT = std::chrono::duration<double>;
+  StopWatch swatch;
+  swatch.start();
+  auto elapsed = swatch.lapse<DurationT>();
+  static_assert(std::is_same_v<decltype(elapsed), DurationT>);
+
+  auto total_elapsed = swatch.stop<DurationT>();
+  static_assert(std::is_same_v<decltype(total_elapsed), DurationT>);
+}
+
+}  // namespace heph::utils::timing::tests
