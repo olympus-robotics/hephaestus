ca37f54b359e35ad4c49ac725dfbdda5bc88cc4c
Filippo Brizzi <filippobrizzi@gmail.com>
2024-12-06 12:04:01 +0000

    fix influxdb

--- modules/telemetry/telemetry_influxdb_sink/examples/influxdb_measure_example.cpp
+++ modules/telemetry/telemetry_influxdb_sink/examples/influxdb_measure_example.cpp
@@ -40,8 +40,8 @@ void run() {
 
   std::uniform_int_distribution<int64_t> duration_dist(MIN_DURATION, MAX_DURATION);
   for (std::size_t counter = 0; !heph::utils::TerminationBlocker::stopRequested(); ++counter) {
-    std::this_thread::sleep_for(std::chrono::milliseconds(duration_dist(mt)));
-    heph::telemetry::record("telemetry_example", "dummy", counter,
+    // std::this_thread::sleep_for(std::chrono::milliseconds(duration_dist(mt)));
+    heph::telemetry::record("telemetry_example", "dummy",
                             DummyMeasure{
                                 .error = heph::random::random<double>(mt),
                                 .counter = static_cast<int64_t>(counter),
@@ -58,12 +58,18 @@ auto main(int argc, const char* argv[]) -> int {
   const heph::utils::StackTrace stack;
 
   try {
-    auto influxdb_sink = heph::telemetry_sink::InfluxDBSink::create(
-        { .url = "localhost:8087", .token = "my-super-secret-auth-token", .database = "hephaestus" });
+    auto influxdb_sink = heph::telemetry_sink::InfluxDBSink::create({ .url = "localhost:8099",
+                                                                      .token = "my-super-secret-auth-token",
+                                                                      .database = "hephaestus",
+                                                                      .flush_rate_hz = 10 });
     heph::telemetry::registerMetricSink(std::move(influxdb_sink));
 
-    auto future = std::async(std::launch::async, &telemetry_example::run);
-    future.get();
+    auto spinner = heph::concurrency::Spinner(
+        heph::concurrency::Spinner::createNeverStoppingCallback([]() { telemetry_example::run(); }), 1);
+
+    spinner.start();
+    heph::utils::TerminationBlocker::waitForInterrupt();
+    spinner.stop().get();
 
   } catch (std::exception& e) {
     fmt::println(stderr, "Execution terminated with exception: {}", e.what());
--- modules/telemetry/telemetry_influxdb_sink/src/influxdb_metric_sink.cpp
+++ modules/telemetry/telemetry_influxdb_sink/src/influxdb_metric_sink.cpp
@@ -4,6 +4,7 @@
 
 #include "hephaestus/telemetry_influxdb_sink/influxdb_metric_sink.h"
 
+#include <cstddef>
 #include <cstdint>
 #include <exception>
 #include <memory>
@@ -19,6 +20,7 @@
 #include <absl/strings/ascii.h>
 #include <fmt/format.h>
 
+#include "hephaestus/concurrency/spinner.h"
 #include "hephaestus/telemetry/metric_sink.h"
 
 namespace heph::telemetry_sink {
@@ -70,12 +72,26 @@ InfluxDBSink::InfluxDBSink(InfluxDBSinkConfig config) : config_(std::move(config
   const auto url = fmt::format(URI_FORMAT, config_.token, config_.url, config_.database);
   LOG(INFO) << fmt::format("Connecting to InfluxDB at {}", url);
   influxdb_ = influxdb::InfluxDBFactory::Get(url);
-  if (config_.batch_size > 0) {
-    influxdb_->batchOf(config_.batch_size);
+
+  if (config_.flush_rate_hz.has_value()) {
+    // NOTE: we define a very large number for the batch size as we want to flush at a fixed rate.
+    static constexpr std::size_t DEFAULT_BATCH_SIZE = 1e6;
+    influxdb_->batchOf(DEFAULT_BATCH_SIZE);
+    spinner_ = std::make_unique<concurrency::Spinner>(
+        [this]() {
+          influxdb_->flushBatch();
+          return concurrency::Spinner::SpinResult::CONTINUE;
+        },
+        config_.flush_rate_hz.value());
+    spinner_->start();
+  } else if (config_.batch_size.has_value()) {
+    influxdb_->batchOf(config_.batch_size.value());
   }
 }
 
-InfluxDBSink::~InfluxDBSink() = default;
+InfluxDBSink::~InfluxDBSink() {
+  spinner_->stop().get();
+}
 
 void InfluxDBSink::send(const telemetry::Metric& entry) {
   auto point = createInfluxdbPoint(entry);
