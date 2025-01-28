9f2ff3177bf45ba0d7cfbef5b5ebbc0fb6ec5388
Lieven van der Heide <lievenvanderheide@gmail.com>
2025-01-28 14:59:19 +0100

    Added a heph::Overloads struct to be used with std::visit (#261)
    This change allows you to handle the various alternatives like this:
    
    ```
    std::visit(heph::Overloads {
        [](const FirstAlternative& val) {
        },
        [](const SecondAlternative& val) {
        },
      },
      val);
    ```
    
    # Description
    
    
    ## Type of change
    Please delete options that are not relevant.
    
    - Bug fix (non-breaking change which fixes an issue)
    - New feature (non-breaking change which adds functionality)
    - Breaking change (fix or feature that would cause existing
    functionality to not work as expected)
    
    ## Checklist before requesting a review
    - [x] I have performed a self-review of my code.
    - [x] If it is a core feature, I have added thorough tests.
    - [ ] If this is a new component I have added examples.
    - [ ] I updated the README and related documentation.
    
    ---------
    
    Co-authored-by: Lieven van der Heide <vanderheide@filics.eu>

---
+++ modules/utils/tests/variant_tests.cpp
@@ -0,0 +1,31 @@
+//=================================================================================================
+// Copyright (C) 2025 HEPHAESTUS Contributors
+//=================================================================================================
+#include <exception>
+#include <variant>
+
+#include <fmt/format.h>
+#include <gmock/gmock.h>
+#include <gtest/gtest.h>
+
+#include "hephaestus/utils/variant.h"
+
+// NOLINTNEXTLINE(google-build-using-namespace)
+using namespace ::testing;
+
+namespace heph::tests {
+TEST(Variant, VariantOverloads) {
+  using Variant = std::variant<std::string, int, float>;
+
+  auto overloads = Overloads{
+    [](const std::string& value) { return fmt::format("Holds string \"{}\"", value); },
+    [](const int value) { return fmt::format("Holds int {}", value); },
+    [](const float value) { return fmt::format("Holds float {}", value); },
+  };
+
+  EXPECT_EQ(std::visit(overloads, Variant("foo")), "Holds string \"foo\"");
+  EXPECT_EQ(std::visit(overloads, Variant(2)), "Holds int 2");
+  EXPECT_EQ(std::visit(overloads, Variant(3.0f)), "Holds float 3");
+}
+
+}  // namespace heph::tests
