6d53d42ec6dd28af03bc50f43942acf3db85caef
Marius Fehr <mbf@voliro.com>
2025-02-04 18:06:27 +0100

    undo unnecessary switch from CHECK to ABSL_CHECK

--- modules/utils/include/hephaestus/utils/exception.h
+++ modules/utils/include/hephaestus/utils/exception.h
@@ -13,7 +13,6 @@
 #include <absl/log/check.h>
 
 #ifdef DISABLE_EXCEPTIONS
-#include <absl/log/absl_check.h>
 #include <fmt/format.h>
 #endif
 
@@ -44,7 +43,7 @@ constexpr void throwException(const std::string& message,
   throw T{ message, location };
 #else
   auto e = T{ message, location };
-  ABSL_CHECK(false) << fmt::format("[ERROR {}] {} at {}:{}", e.what(), message, location.file_name(),
+  CHECK(false) << fmt::format("[ERROR {}] {} at {}:{}", e.what(), message, location.file_name(),
                                    location.line());
 #endif
 }
@@ -65,7 +64,7 @@ constexpr void throwExceptionIf(bool condition, const std::string& message,
   }
 #else
   auto e = T{ message, location };
-  ABSL_CHECK(!condition) << fmt::format("[ERROR {}] {} at {}:{}", e.what(), message, location.file_name(),
+  CHECK(!condition) << fmt::format("[ERROR {}] {} at {}:{}", e.what(), message, location.file_name(),
                                         location.line());
 #endif
 }
