6338686963d865d5e3906ba4f8323c7ec758f162
Filippo Brizzi <filippobrizzi@gmail.com>
2025-02-05 16:20:54 +0000

    add fake clock

--- modules/utils/src/timing/fake_clock.cpp
+++ modules/utils/src/timing/fake_clock.cpp
@@ -2,4 +2,4 @@
 // Copyright (C) 2023-2024 HEPHAESTUS Contributors
 //=================================================================================================
 
-#include "hephaestus/utils/timing/fake_watch.h"
+#include "hephaestus/utils/timing/fake_clock.h"
