8c8adbc15a976c8352f30efeb431042f0c1680fa
Marius Fehr <mbf@voliro.com>
2025-02-06 19:39:00 +0100

    ws_bridge: bazel build success

--- bazel/repositories.bzl
+++ bazel/repositories.bzl
@@ -50,3 +50,13 @@ def foreign_cc_repositories():
         strip_prefix = "reflect-cpp-" + REFLECT_CPP_VERSION + "-cmake-3.22.1",
         sha256 = "63fd52189f7df9c5648c5479e7af9377271a18f65a30a1a45fe160f8e0cf787a",
     )
+    
+    WS_PROTOCOL_VERSION = "1.4.0"
+    WS_PROTOCOL_TAG = "releases/cpp/v" + WS_PROTOCOL_VERSION
+    http_archive(
+        name = "ws_protocol",
+        build_file = ":foreign_cc/ws_protocol.BUILD",
+        urls = ["https://github.com/foxglove/ws-protocol/archive/refs/tags/{tag}.zip".format(tag = WS_PROTOCOL_TAG)],
+        strip_prefix = "ws-protocol-releases-cpp-v" + WS_PROTOCOL_VERSION,
+        sha256 = "ae2efd02bb3ace0a570fdabab4efaaf6e153d91e35185c268ee256457e598885",
+    )
--- modules/ws_bridge/include/hephaestus/ws_bridge/ws_bridge.h
+++ modules/ws_bridge/include/hephaestus/ws_bridge/ws_bridge.h
@@ -4,6 +4,27 @@
 
 #pragma once
 
+#include <memory>
+#include <mutex>
+#include <set>
+#include <string>
+#include <unordered_map>
+#include <unordered_set>
+#include <utility>
+#include <vector>
+
+#include <absl/log/check.h>
+#include <fmt/core.h>
+#include <magic_enum.hpp>
+
+#include <foxglove/websocket/callback_queue.hpp>
+// #include <foxglove_bridge/foxglove/websocket.hpp>
+// #include <foxglove_bridge/message_definition_cache.hpp>
+#include <foxglove/websocket/regex_utils.hpp>
+#include <foxglove/websocket/server_factory.hpp>
+#include <foxglove/websocket/server_interface.hpp>
+#include <websocketpp/common/connection_hdl.hpp>
+
 namespace heph::ws_bridge{
   /// \addtogroup ws_bridge ws_bridge module
   /// @{
