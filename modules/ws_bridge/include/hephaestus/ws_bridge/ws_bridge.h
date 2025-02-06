9c491c168b6e388628469c7c879a4a10a1d9ecf1
Marius Fehr <mbf@voliro.com>
2025-02-06 16:20:49 +0100

    create_module.py ws_bridge

---
+++ modules/ws_bridge/include/hephaestus/ws_bridge/ws_bridge.h
@@ -0,0 +1,14 @@
+//=================================================================================================
+// Copyright (C) 2023-2024 HEPHAESTUS Contributors
+//=================================================================================================
+
+#pragma once
+
+namespace heph::ws_bridge{
+  /// \addtogroup ws_bridge ws_bridge module
+  /// @{
+
+  /// TODO Implement module interface here
+
+  ///@}
+}
---
+++ modules/ws_bridge/tests/tests.cpp
@@ -0,0 +1,15 @@
+//=================================================================================================
+// Copyright (C) 2023-2024 HEPHAESTUS Contributors
+//=================================================================================================
+
+#include <gtest/gtest.h>
+
+// NOLINTNEXTLINE(google-build-using-namespace)
+using namespace ::testing;
+
+namespace heph::ws_bridge::tests {
+
+TEST(Example, Example) {
+  EXPECT_TRUE(false);
+}
+}
