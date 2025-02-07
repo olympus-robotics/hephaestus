60a1fb295e0e2fab66a0fcc7c249234313f8200f
Marius Fehr <mbf@voliro.com>
2025-02-07 13:07:31 +0100

    fix EndpointInfo naming and allow soft-failure of type retrieval

--- modules/ipc/src/zenoh/dynamic_subscriber.cpp
+++ modules/ipc/src/zenoh/dynamic_subscriber.cpp
@@ -85,7 +85,15 @@ void DynamicSubscriber::onPublisherAdded(const EndpointInfo& info) {
   }
 
   if (init_subscriber_cb_) {
-    init_subscriber_cb_(info.topic, *type_info);
+    auto type_info = topic_db_->getTypeInfo(info.topic);
+
+    if (!type_info.has_value()) {
+      heph::log(heph::ERROR, "failed to get type info for topic", "topic", info.topic);
+      return;
+    }
+
+    init_subscriber_cb_(info.topic, type_info.value());
+    optional_type_info = std::move(type_info);
   }
 
   heph::log(heph::DEBUG, "create subscriber", "topic", info.topic);
