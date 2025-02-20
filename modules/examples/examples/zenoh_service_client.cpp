40fb97986d71856a5cd91dfbe8c4229f32e0ad05
Thomas Heller <heller@filics.eu>
2025-02-20 11:16:23 +0100

    Make linter happy

--- modules/examples/examples/zenoh_service_client.cpp
+++ modules/examples/examples/zenoh_service_client.cpp
@@ -38,7 +38,7 @@ auto main(int argc, const char* argv[]) -> int {
     const auto args = std::move(desc).parse(argc, argv);
     auto [session_config, topic_config] = heph::ipc::zenoh::parseProgramOptions(args);
 
-    auto session = heph::ipc::zenoh::createSession(std::move(session_config));
+    auto session = heph::ipc::zenoh::createSession(session_config);
 
     static constexpr auto K_TIMEOUT = std::chrono::seconds(10);
     const auto query =
