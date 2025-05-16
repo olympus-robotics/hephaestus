8c55ab27a253d019643b534b8f91bfdf9b13d1ac
Joe <jottgee@posteo.de>
2025-05-16 17:47:27 +0200

    add rate limiter

---
+++ modules/utils/src/timing/rate_limiter.cpp
@@ -0,0 +1,23 @@
+//=================================================================================================
+// Copyright (C) 2023-2024 HEPHAESTUS Contributors
+//=================================================================================================
+
+#include "hephaestus/utils/timing/rate_limiter.h"
+
+#include <chrono>
+#include <functional>
+
+namespace heph::utils::timing {
+
+RateLimiter::RateLimiter(std::chrono::milliseconds rate) : rate_{ rate } {
+}
+
+void RateLimiter::operator()(const std::function<void()>& callback) {
+  auto now = ClockT::now();
+  if (now - timestamp_last_call_ > rate_) {
+    callback();
+    timestamp_last_call_ = now;
+  }
+}
+
+}  // namespace heph::utils::timing
