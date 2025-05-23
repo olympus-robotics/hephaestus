a3819ff7923eb9e5bdebe6655af78b9b8fc2d1f9
Thomas Heller <heller@filics.eu>
2025-05-23 10:29:56 +0200

    Code cleanup

--- modules/conduit/include/hephaestus/conduit/detail/node_base.h
+++ modules/conduit/include/hephaestus/conduit/detail/node_base.h
@@ -1,13 +1,60 @@
+//=================================================================================================
+// Copyright (C) 2023-2024 HEPHAESTUS Contributors
+//=================================================================================================
 
 #pragma once
 
+#include <chrono>
 #include <string>
+#include <string_view>
+
+// Forward declarations
+namespace heph::conduit {
+class NodeEngine;
+}
 
 namespace heph::conduit::detail {
+
 class NodeBase {
 public:
+  static constexpr std::string_view MISSED_DEADLINE_WARNING = "Missed deadline";
+
   virtual ~NodeBase() = default;
   [[nodiscard]] virtual auto nodeName() const -> std::string = 0;
+  [[nodiscard]] virtual auto nodePeriod() -> std::chrono::nanoseconds = 0;
+
+  auto engine() -> NodeEngine& {
+    return *engine_;
+  }
+
+  [[nodiscard]] auto engine() const -> NodeEngine const& {
+    return *engine_;
+  }
+
+protected:
+  void updateExecutionTime(std::chrono::nanoseconds duration);
+  auto lastPeriodDuration() -> std::chrono::nanoseconds;
+
+private:
+  friend class heph::conduit::NodeEngine;
+  friend class ExecutionStopWatch;
+  NodeEngine* engine_{ nullptr };
+  std::chrono::nanoseconds last_execution_duration_{};
+};
+
+class ExecutionStopWatch {
+public:
+  explicit ExecutionStopWatch(NodeBase* self);
+  ~ExecutionStopWatch() noexcept;
+
+  ExecutionStopWatch(ExecutionStopWatch const&) = delete;
+  auto operator=(ExecutionStopWatch const&) -> ExecutionStopWatch& = delete;
+  ExecutionStopWatch(ExecutionStopWatch&&) = delete;
+  auto operator=(ExecutionStopWatch&&) -> ExecutionStopWatch& = delete;
+
+private:
+  NodeBase* self_;
+  std::chrono::high_resolution_clock::time_point start_;
 };
 
 }  // namespace heph::conduit::detail
--- modules/conduit/include/hephaestus/conduit/node.h
+++ modules/conduit/include/hephaestus/conduit/node.h
@@ -8,7 +8,6 @@
 #include <cstddef>
 #include <optional>
 #include <string>
-#include <string_view>
 #include <variant>
 
 #include <stdexec/__detail/__senders_core.hpp>
@@ -17,8 +16,6 @@
 #include "hephaestus/conduit/detail/node_base.h"
 #include "hephaestus/conduit/detail/output_connections.h"
 #include "hephaestus/conduit/node_engine.h"
-#include "hephaestus/telemetry/log.h"
-#include "hephaestus/telemetry/log_sink.h"
 
 namespace heph::conduit {
 namespace detail {
@@ -28,7 +25,7 @@ class InputBase;
 }  // namespace detail
 
 template <typename OperationT, typename OperationDataT = detail::Unused>
-class Node : detail::NodeBase {
+class Node : public detail::NodeBase {
   static constexpr bool HAS_PERIOD_CONSTANT = requires(OperationT&) { OperationT::PERIOD; };
   static constexpr bool HAS_PERIOD_NULLARY = requires(OperationT&) { OperationT::period(); };
   static constexpr bool HAS_PERIOD_ARG = requires(OperationT& op) { OperationT::period(op); };
@@ -48,15 +45,6 @@ class Node : detail::NodeBase {
 public:
   static constexpr bool HAS_PERIOD = HAS_PERIOD_CONSTANT || HAS_PERIOD_NULLARY || HAS_PERIOD_ARG;
   static constexpr bool HAS_NAME = HAS_NAME_CONSTANT || HAS_NAME_NULLARY || HAS_NAME_ARG;
-  static constexpr std::string_view MISSED_DEADLINE_WARNING = "Missed deadline";
-
-  auto engine() -> NodeEngine& {
-    return *engine_;
-  }
-
-  [[nodiscard]] auto engine() const -> NodeEngine const& {
-    return *engine_;
-  }
 
   auto data() const -> OperationDataT const& {
     return data_.value();
@@ -66,22 +54,14 @@ public:
     return data_.value();
   }
 
-  [[nodiscard]] auto nodeName() const -> std::string final {
-    if constexpr (HAS_NAME_ARG) {
-      return std::string{ OperationT::name(operation()) };
-    } else if constexpr (HAS_NAME_NULLARY) {
-      return std::string{ OperationT::name() };
-    } else if constexpr (HAS_NAME_CONSTANT) {
-      return std::string{ OperationT::NAME };
-    } else {
-      return std::string{ typeid(OperationT).name() };
-    }
-  }
+  [[nodiscard]] auto nodeName() const -> std::string final;
+
+  [[nodiscard]] auto nodePeriod() -> std::chrono::nanoseconds final;
 
 private:
   auto invokeOperation() {
     return [this]<typename... Ts>(Ts&&... ts) {
-      ExecutionStopWatch stop_watch{ this };
+      detail::ExecutionStopWatch stop_watch{ this };
       static_assert(HAS_EXECUTE_ARG<Ts...> || HAS_EXECUTE_NULLARY<Ts...>,
                     "No valid execute function available");
       if constexpr (HAS_EXECUTE_ARG<Ts...>) {
@@ -111,7 +91,7 @@ private:
   }
 
   auto triggerExecute() {
-    return executeSender() | outputs_->propagate(*engine_);
+    return executeSender() | outputs_->propagate(engine());
   }
 
   template <typename Input>
@@ -130,27 +110,9 @@ private:
   auto operationTrigger() {
     auto schedule_trigger = [&] {
       if constexpr (HAS_PERIOD) {
-        auto period = [this]() {
-          if constexpr (HAS_PERIOD_NULLARY) {
-            return OperationT::period();
-          } else if constexpr (HAS_PERIOD_ARG) {
-            return OperationT::period(operation());
-          } else {
-            return OperationT::PERIOD;
-          }
-        }();
-        std::chrono::nanoseconds start_after = period - last_execution_duration_;
-        // We attempt to avoid drift by adapting the expiry time with the
-        // measured execution time of the last execution duration.
-        // If execution took longer, we should scheduler immediately (delay of 0).
-        if (last_execution_duration_ >= period) {
-          heph::log(heph::WARN, std::string{ MISSED_DEADLINE_WARNING }, "node", nodeName(), "period", period,
-                    "duration", last_execution_duration_);
-          start_after = std::chrono::nanoseconds{ 0 };
-        }
-        return engine_->scheduler().scheduleAfter(start_after);
+        return engine().scheduler().scheduleAfter(lastPeriodDuration());
       } else {
-        return engine_->scheduler().schedule();
+        return engine().scheduler().schedule();
       }
     };
     return stdexec::just() | stdexec::let_value(schedule_trigger) | stdexec::let_value([this] {
@@ -166,38 +128,40 @@ private:
            });
   }
 
-  void updateExecutionTime(std::chrono::nanoseconds duration) noexcept {
-    last_execution_duration_ = duration;
-  }
-
 private:
   friend class NodeEngine;
 
   template <typename InputT, typename T, std::size_t Depth>
   friend class detail::InputBase;
 
-  class ExecutionStopWatch {
-  public:
-    explicit ExecutionStopWatch(Node* self) : self_(self), start_(std::chrono::high_resolution_clock::now()) {
-    }
-    ~ExecutionStopWatch() noexcept {
-      auto end = std::chrono::high_resolution_clock::now();
-      self_->updateExecutionTime(end - start_);
-    }
-
-    ExecutionStopWatch(ExecutionStopWatch const&) = delete;
-    auto operator=(ExecutionStopWatch const&) -> ExecutionStopWatch& = delete;
-    ExecutionStopWatch(ExecutionStopWatch&&) = delete;
-    auto operator=(ExecutionStopWatch&&) -> ExecutionStopWatch& = delete;
-
-  private:
-    Node* self_;
-    std::chrono::high_resolution_clock::time_point start_;
-  };
-
   std::optional<detail::OutputConnections> outputs_;
-  std::chrono::nanoseconds last_execution_duration_{};
-  NodeEngine* engine_{ nullptr };
   std::optional<OperationDataT> data_;
 };
+
+template <typename OperationT, typename OperationDataT>
+inline auto Node<OperationT, OperationDataT>::nodePeriod() -> std::chrono::nanoseconds {
+  if constexpr (HAS_PERIOD_NULLARY) {
+    return OperationT::period();
+  } else if constexpr (HAS_PERIOD_ARG) {
+    return OperationT::period(operation());
+  } else if constexpr (HAS_PERIOD_CONSTANT) {
+    return OperationT::PERIOD;
+  } else {
+    return {};
+  }
+}
+
+template <typename OperationT, typename OperationDataT>
+inline auto Node<OperationT, OperationDataT>::nodeName() const -> std::string {
+  if constexpr (HAS_NAME_ARG) {
+    return std::string{ OperationT::name(operation()) };
+  } else if constexpr (HAS_NAME_NULLARY) {
+    return std::string{ OperationT::name() };
+  } else if constexpr (HAS_NAME_CONSTANT) {
+    return std::string{ OperationT::NAME };
+  } else {
+    return std::string{ typeid(OperationT).name() };
+  }
+}
+
 }  // namespace heph::conduit
