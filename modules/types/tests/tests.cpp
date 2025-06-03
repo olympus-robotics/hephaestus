72b92f1fd0732fd54e667c88a18449c401294168
lhruby <hruby@filics.eu>
2025-06-03 12:01:35 +0000

    Fix.

--- modules/types/tests/tests.cpp
+++ modules/types/tests/tests.cpp
@@ -4,9 +4,9 @@
 
 #include <cstdint>
 
+#include <absl/hash/hash_testing.h>
 #include <gtest/gtest.h>
 
-#include "absl/hash/hash_testing.h"
 #include "hephaestus/random/random_number_generator.h"
 #include "hephaestus/types/bounds.h"
 #include "hephaestus/types/dummy_type.h"
@@ -17,7 +17,9 @@ namespace heph::types::tests {
 using IntegerBoundsT = Bounds<int32_t>;
 using FloatingPointBoundsT = Bounds<float>;
 
-/* --- Test all custom structs which support creation via a random member function --- */
+//=================================================================================================
+// Test all custom structs which support creation via a random member function
+//=================================================================================================
 template <class T>
 class TypeTests : public ::testing::Test {};
 using TypeImplementations = ::testing::Types<IntegerBoundsT, FloatingPointBoundsT, DummyType, UuidV4>;
@@ -25,23 +27,27 @@ using TypeImplementations = ::testing::Types<IntegerBoundsT, FloatingPointBounds
 TYPED_TEST_SUITE(TypeTests, TypeImplementations);
 
 TYPED_TEST(TypeTests, RandomUnequalTest) {
-  auto [mt, mt_copy] = heph::random::createPairOfIdenticalRNGs();
+  auto [mt, mt_copy] = random::createPairOfIdenticalRNGs();
 
   EXPECT_EQ(TypeParam::random(mt), TypeParam::random(mt_copy));  // Ensure determinism
   EXPECT_NE(TypeParam::random(mt), TypeParam::random(mt));       // Ensure randomness
 }
 
-/* --- Test types which support hashing --- */
+//=================================================================================================
+// Test types which support hashing
+//=================================================================================================
 template <class T>
 class HashingTest : public ::testing::Test {};
 using HashingImplementations = ::testing::Types<UuidV4>;
+
 TYPED_TEST_SUITE(HashingTest, HashingImplementations);
+
 TYPED_TEST(HashingTest, TestHash) {
-  auto mt = heph::random::createRNG();
+  auto mt = random::createRNG();
 
   EXPECT_TRUE(absl::VerifyTypeImplementsAbslHashCorrectly({
       TypeParam(),
-      types_random::random<TypeParam>(mt),
+      TypeParam::random(mt),
   }));
 }
 
