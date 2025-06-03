2824d11a5f40cb68539d3f266b7b455ae9784e93
Lorenz Hruby <hruby@filics.eu>
2025-06-03 16:06:55 +0200

    Add hashing capabilities to Uuid v4 and hash testing. (#353)
    # Description
    Add hashing capabilities to Uuid v4 and hash testing.
    
    ## Type of change
    - New feature (non-breaking change which adds functionality)
    
    ## Checklist before requesting a review
    - [x] I have performed a self-review of my code.
    - [x] If it is a core feature, I have added thorough tests.
    - [x] If this is a new component I have added examples.
    - [x] I updated the README and related documentation.

--- modules/types/tests/tests.cpp
+++ modules/types/tests/tests.cpp
@@ -4,6 +4,7 @@
 
 #include <cstdint>
 
+#include <absl/hash/hash_testing.h>
 #include <gtest/gtest.h>
 
 #include "hephaestus/random/random_number_generator.h"
@@ -16,7 +17,9 @@ namespace heph::types::tests {
 using IntegerBoundsT = Bounds<int32_t>;
 using FloatingPointBoundsT = Bounds<float>;
 
-/* --- Test all custom structs which support creation via a random member function --- */
+//=================================================================================================
+// Test all custom structs which support creation via a random member function
+//=================================================================================================
 template <class T>
 class TypeTests : public ::testing::Test {};
 using TypeImplementations = ::testing::Types<IntegerBoundsT, FloatingPointBoundsT, DummyType, UuidV4>;
@@ -24,10 +27,28 @@ using TypeImplementations = ::testing::Types<IntegerBoundsT, FloatingPointBounds
 TYPED_TEST_SUITE(TypeTests, TypeImplementations);
 
 TYPED_TEST(TypeTests, RandomUnequalTest) {
-  auto [mt, mt_copy] = heph::random::createPairOfIdenticalRNGs();
+  auto [mt, mt_copy] = random::createPairOfIdenticalRNGs();
 
   EXPECT_EQ(TypeParam::random(mt), TypeParam::random(mt_copy));  // Ensure determinism
   EXPECT_NE(TypeParam::random(mt), TypeParam::random(mt));       // Ensure randomness
+}
+
+//=================================================================================================
+// Test types which support hashing
+//=================================================================================================
+template <class T>
+class HashingTest : public ::testing::Test {};
+using HashingImplementations = ::testing::Types<UuidV4>;
+
+TYPED_TEST_SUITE(HashingTest, HashingImplementations);
+
+TYPED_TEST(HashingTest, TestHash) {
+  auto mt = random::createRNG();
+
+  EXPECT_TRUE(absl::VerifyTypeImplementsAbslHashCorrectly({
+      TypeParam(),
+      TypeParam::random(mt),
+  }));
 }
 
 }  // namespace heph::types::tests
