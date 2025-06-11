baa698bd007d6459a06eac74b8c0d72fefe4e234
Filippo Brizzi <filippobrizzi@gmail.com>
2025-06-11 08:11:29 +0000

    improve name

--- modules/conduit/include/hephaestus/conduit/zenoh_nodes.h
+++ modules/conduit/include/hephaestus/conduit/zenoh_nodes.h
@@ -46,7 +46,7 @@ struct ZenohPublisherNode : conduit::Node<ZenohPublisherNode<T>, ZenohPublisherO
   QueuedInput<T> input{ this, std::string{ std::string_view{ InputName } } };
 
   static auto name() -> std::string_view {
-    static constexpr auto NAME = utils::string::StringLiteral{ "zenoh_publisher_" } + InputName;
+    static constexpr auto NAME = utils::string::StringLiteral{ "zenoh_publisher:" } + InputName;
     return std::string_view{ NAME };
   }
 
