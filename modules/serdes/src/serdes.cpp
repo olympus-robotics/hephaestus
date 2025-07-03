7d1f5a653991b2013305513f3d53382100a5103d
Filippo Brizzi <filippobrizzi@gmail.com>
2025-07-03 08:14:29 +0000

    more fixed for gcc

--- modules/serdes/src/serdes.cpp
+++ modules/serdes/src/serdes.cpp
@@ -2,6 +2,6 @@
 // Copyright (C) 2023-2024 HEPHAESTUS Contributors
 //=================================================================================================
 
+#ifndef __GNUC__  // For some reason GCC fails to compile as it tries to instantiate the template
 #include "hephaestus/serdes/serdes.h"  // NOLINT(misc-include-cleaner)
-
-namespace heph::serdes {}  // namespace heph::serdes
+#endif
