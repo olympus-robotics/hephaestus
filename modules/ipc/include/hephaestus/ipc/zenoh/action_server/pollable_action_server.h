51364ac64070202df10b2a08d8a4a004e2d9f4d3
Maximilian Geisslinger <geisslinger@filics.eu>
2025-07-14 12:08:06 +0200

    reset action server status

--- modules/ipc/include/hephaestus/ipc/zenoh/action_server/pollable_action_server.h
+++ modules/ipc/include/hephaestus/ipc/zenoh/action_server/pollable_action_server.h
@@ -134,6 +134,7 @@ PollableActionServer<RequestT, StatusT, ReplyT>::PollableActionServer(SessionPtr
 
             if (status_.has_value()) {
               status_publisher.publish(*std::move(status_));
+              status_.reset();
             }
           }
 
