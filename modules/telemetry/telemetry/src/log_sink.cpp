2b7fd2a0a806059f1b07d5214f87346bceb25879
Filippo Brizzi <filippobrizzi@gmail.com>
2025-09-30 15:53:27 +0000

    add scope

--- modules/telemetry/telemetry/src/log_sink.cpp
+++ modules/telemetry/telemetry/src/log_sink.cpp
@@ -9,14 +9,24 @@
 
 #include "hephaestus/utils/utils.h"
 
+namespace heph::telemetry {
+namespace {
+[[nodiscard]] auto getModuleFromScope() -> std::string {
+  const auto& module_stack = getModulesStack();
+  return module_stack.empty() ? "global" : fmt::format("{}", fmt::join(module_stack, "/"));
+}
+}  // namespace
+
 // Instead of deactivating the following check we could globally set AllowPartialMove to true
 // NOLINTNEXTLINE(cppcoreguidelines-rvalue-reference-param-not-moved)
-heph::telemetry::LogEntry::LogEntry(LogLevel level_in, MessageWithLocation&& message_in)
+LogEntry::LogEntry(LogLevel level_in, MessageWithLocation&& message_in)
   : level{ level_in }
   , message{ std::move(message_in.value) }
   , location{ message_in.location }
   , thread_id{ std::this_thread::get_id() }
   , time{ LogEntry::ClockT::now() }
-  , hostname{ heph::utils::getHostName() }
+  , hostname{ utils::getHostName() }
+  , module{ getModuleFromScope() }
   , stack_trace{ std::nullopt } {
 }
+}  // namespace heph::telemetry
