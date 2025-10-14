6c7113e94ab1bc25bc92af5cf9b331675a17c092
Henti Smith <smith@filics.eu>
2025-10-14 15:46:34 +0200

    Reverted recordMatrics anbd added extend-words rules

--- modules/concurrency/src/spinner.cpp
+++ modules/concurrency/src/spinner.cpp
@@ -43,7 +43,7 @@ public:
   }
 
   // Record
-  void recordMatrix() {
+  void recordMatrics() {
     if (!component_name_.has_value()) {
       return;
     }
@@ -135,7 +135,7 @@ void Spinner::spin() {
         break;
       }
       spinner_telemetry.timeCallback();
-      spinner_telemetry.recordMatrix();
+      spinner_telemetry.recordMatrics();
 
       if (spin_period_.has_value()) {  // Throttle spinner to a fixed period if spin_period_ is provided
         std::unique_lock<std::mutex> lock(mutex_);
