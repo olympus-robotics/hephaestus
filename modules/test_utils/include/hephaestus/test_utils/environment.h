1db9f58361646891ff167a190cb2e52f06e7f0bd
Filippo Brizzi <filippobrizzi@gmail.com>
2025-11-21 10:38:29 +0000

    work

--- modules/test_utils/include/hephaestus/test_utils/environment.h
+++ modules/test_utils/include/hephaestus/test_utils/environment.h
@@ -2,9 +2,50 @@
 // |   This file is part of the FORKIFY project by FILICS GmbH. All rights reserved.   |
 // =====================================================================================
 
+#include <memory>
 #include <random>
 
+#include <gtest/gtest.h>
+
+#include "hephaestus/error_handling/panic.h"
+#include "hephaestus/random/random_number_generator.h"
+#include "hephaestus/telemetry/log/log.h"
+#include "hephaestus/telemetry/log/sinks/absl_sink.h"
+#include "hephaestus/telemetry/metrics/metric_record.h"
+#include "hephaestus/utils/stack_trace.h"
+
 namespace heph::test_utils {
+namespace internal {
+class DefaultEnvironment : public ::testing::Environment {
+public:
+  DefaultEnvironment() = default;
+  DefaultEnvironment(const DefaultEnvironment&) = delete;
+  DefaultEnvironment(DefaultEnvironment&&) = delete;
+  auto operator=(const DefaultEnvironment&) -> DefaultEnvironment& = delete;
+  auto operator=(DefaultEnvironment&&) -> DefaultEnvironment& = delete;
+
+  ~DefaultEnvironment() override {
+    telemetry::flushMetrics();
+    telemetry::flushLogEntries();
+  };
+
+  void SetUp() override {
+    telemetry::registerLogSink(std::make_unique<telemetry::AbslLogSink>(INFO));
+  }
+
+  [[nodiscard]] auto mt() -> std::mt19937_64& {
+    return mt_;
+  }
+
+private:
+  const utils::StackTrace trace_;
+  std::mt19937_64 mt_{ heph::random::createRNG() };
+  const error_handling::PanicAsExceptionScope panic_scope_;
+};
+
+static const DefaultEnvironment* default_test_environments = nullptr;  // NOLINT
+
+}  // namespace internal
 
 [[nodiscard]] auto mt() -> std::mt19937_64&;
 
