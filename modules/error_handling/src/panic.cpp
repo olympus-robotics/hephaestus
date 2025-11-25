64ef63fa48af2c9b32b72e2d270b81fd27814551
Vittorio Romeo <vittorio.romeo@outlook.com>
2025-11-25 15:30:45 +0100

    Improve panic.h compilation overhead via isolation to cpp (#443)
    # Description
    
    Hides `panic` implementation details in `panic.cpp` to avoid including
    heavy headers transitively from `panic.h`. This is generally a good
    pattern to follow for templates: extract the part that doesn't depend on
    the template parameters into a source file, and call the extracted
    function from the template.
    
    ## Type of change
    
    - New feature (non-breaking change which adds functionality)
    
    
    ## Checklist before requesting a review
    - [x] I have performed a self-review of my code.
    - [x] If it is a core feature, I have added thorough tests.
    - [x] If this is a new component I have added examples.
    - [x] I updated the README and related documentation.

--- modules/error_handling/src/panic.cpp
+++ modules/error_handling/src/panic.cpp
@@ -5,6 +5,12 @@
 #include "hephaestus/error_handling/panic.h"
 
 #include <cstddef>
+#include <exception>
+#include <source_location>
+#include <string>
+
+#include "hephaestus/telemetry/log/log.h"
+#include "hephaestus/utils/string/string_utils.h"
 
 namespace heph::error_handling {
 namespace {
@@ -25,5 +31,23 @@ PanicAsExceptionScope::~PanicAsExceptionScope() {
 auto panicAsException() -> bool {
   return panic_as_exception_counter > 0;
 }
+
+namespace detail {
+
+void panicImpl(const std::source_location& location, const std::string& formatted_message) {
+  auto location_str = std::string(utils::string::truncate(location.file_name(), "modules")) + ":" +
+                      std::to_string(location.line());
+
+  log(ERROR, "program terminated with panic", "error", formatted_message, "location", location_str);
+  telemetry::flushLogEntries();
+
+  if (error_handling::panicAsException()) {
+    throw error_handling::PanicException(formatted_message);
+  }
+
+  std::terminate();
+}
+
+}  // namespace detail
 
 }  // namespace heph::error_handling
