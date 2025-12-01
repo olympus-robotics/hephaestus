93fca0e3a1364c865e5be0f53fde71d76a769528
Thomas Heller <heller@filics.eu>
2025-12-01 10:59:59 +0100

    revert to single thread implementation

--- modules/containers/include/hephaestus/containers/fixed_circular_buffer.h
+++ modules/containers/include/hephaestus/containers/fixed_circular_buffer.h
@@ -5,168 +5,19 @@
 #pragma once
 
 #include <array>
-#include <atomic>
 #include <concepts>
 #include <cstddef>
-#include <cstdint>
-#include <new>
 #include <optional>
-#include <type_traits>
 #include <utility>
 
 namespace heph::containers {
-namespace internal {
-template <std::size_t Capacity>
-class UnsynchronizedIndices {
-public:
-  [[nodiscard]] auto empty() const {
-    return size_ == 0;
-  }
-
-  [[nodiscard]] auto full() const {
-    return size_ == Capacity;
-  }
-
-  [[nodiscard]] auto size() const -> std::size_t {
-    return size_;
-  }
-
-  template <typename T, typename U>
-  auto push(std::array<T, Capacity>& data, U&& t) -> bool {
-    if (full()) {
-      return false;
-    }
-
-    data[writeIndex()] = std::forward<U>(t);
-    incrementWriteIndex();
-
-    return true;
-  }
-
-  template <typename T>
-  auto pop(std::array<T, Capacity>& data) -> std::optional<T> {
-    std::optional<T> res;
-    if (empty()) {
-      return res;
-    }
-
-    res.emplace(std::move(data[readIndex()]));
-    incrementReadIndex();
-
-    return res;
-  }
-
-private:
-  [[nodiscard]] auto writeIndex() const -> std::size_t {
-    return write_index_;
-  }
-
-  auto incrementWriteIndex() -> void {
-    write_index_ = (write_index_ + 1) % Capacity;
-    ++size_;
-  }
-
-  [[nodiscard]] auto readIndex() const -> std::size_t {
-    return read_index_;
-  }
-
-  auto incrementReadIndex() -> void {
-    read_index_ = (read_index_ + 1) % Capacity;
-    --size_;
-  }
-
-private:
-  std::size_t read_index_{ 0 };
-  std::size_t write_index_{ 0 };
-  std::size_t size_{ 0 };
-};
-
-template <std::size_t Capacity>
-class SpscIndices {
-public:
-  [[nodiscard]] auto empty() const {
-    return read_index_.load(std::memory_order_acquire) == write_index_.load(std::memory_order_acquire);
-  }
-
-  [[nodiscard]] auto full() const {
-    return size() == Capacity;
-  }
-
-  [[nodiscard]] auto size() const -> std::size_t {
-    auto write_index = write_index_.load(std::memory_order_acquire);
-    auto read_index = read_index_.load(std::memory_order_acquire);
-
-    if (write_index >= read_index) {
-      return write_index - read_index;
-    }
-    return Capacity - read_index + write_index;
-  }
-
-  template <typename T, typename U>
-  auto push(std::array<T, Capacity>& data, U&& t) -> bool {
-    std::size_t write_index{ 0 };
-
-    write_index = write_index_.load(std::memory_order_relaxed);
-
-    // Check if our cached read index is still within range
-    if (cached_read_index_ + Capacity - write_index < 1) {
-      cached_read_index_ = read_index_.load(std::memory_order_acquire);
-      // Check again after loading the real read index
-      if (cached_read_index_ + Capacity - write_index < 1) {
-        return false;
-      }
-    }
-    data[(write_index + 1) % Capacity] = std::forward<U>(t);
-    write_index_.store(write_index + 1, std::memory_order_release);
-    return true;
-  }
-
-  template <typename T>
-  auto pop(std::array<T, Capacity>& data) -> std::optional<T> {
-    std::optional<T> res;
-    std::size_t read_index{ 0 };
-
-    read_index = read_index_.load(std::memory_order_relaxed);
-    if (cached_write_index_ - read_index < 1) {
-      cached_write_index_ = write_index_.load(std::memory_order_acquire);
-      // Check again after loading the real read index
-      if (cached_write_index_ - read_index < 1) {
-        return res;
-      }
-    }
-    res.emplace(std::move(data[(read_index + 1) % Capacity]));
-    read_index_.store(read_index + 1, std::memory_order_release);
-    return res;
-  }
-
-private:
-  // Align atomics to a cache line to avoid false sharing. The cache variables are used
-  // to reduce cache coherency traffic.
-  static constexpr auto CACHE_LINE_SIZE = std::hardware_destructive_interference_size;
-  alignas(CACHE_LINE_SIZE) std::atomic<std::size_t> write_index_{ 0 };
-  alignas(CACHE_LINE_SIZE) std::size_t cached_read_index_{ 0 };
-  alignas(CACHE_LINE_SIZE) std::atomic<std::size_t> read_index_{ 0 };
-  alignas(CACHE_LINE_SIZE) std::size_t cached_write_index_{ 0 };
-};
-
-}  // namespace internal
-
-enum class FixedCircularBufferMode : std::uint8_t {
-  UNSYNCHRONIZED,
-  SPSC,
-};
-
 /// Implementation of a fixed capacity circular buffer
 ///
 /// \tparam T the value type of the elements
 /// \tparam Capacity Maximum capacity. Should be a power of two for efficiency reasons.
-/// \tparam Mode can be used to switch from UNSYNCHRONIZED (default, NOT thread safe) to
-///         a single producer and single consumer (SBSC)
 ///
-/// The SBSC version uses the Implementation presented here:
-/// https://inria.hal.science/hal-00862450/document
-template <typename T, std::size_t Capacity,
-          FixedCircularBufferMode Mode = FixedCircularBufferMode::UNSYNCHRONIZED>
+/// \note This class is not thread safe. If you need to push/pop from differeent threads, protect it with a lock!
+template <typename T, std::size_t Capacity>
 class FixedCircularBuffer {
 public:
   static_assert((Capacity & (Capacity - 1)) == 0, "Capacity should be a power of two.");
@@ -176,8 +27,8 @@ public:
   FixedCircularBuffer(FixedCircularBuffer&& other) noexcept;
   auto operator=(FixedCircularBuffer&& other) noexcept -> FixedCircularBuffer&;
 
-  FixedCircularBuffer(const FixedCircularBuffer& other) = delete;
-  auto operator=(const FixedCircularBuffer& other) -> FixedCircularBuffer& = delete;
+  FixedCircularBuffer(const FixedCircularBuffer& other) = default;
+  auto operator=(const FixedCircularBuffer& other) -> FixedCircularBuffer& = default;
 
   [[nodiscard]] auto empty() const -> bool;
   [[nodiscard]] auto full() const -> bool;
@@ -201,63 +52,81 @@ public:
   [[nodiscard]] auto pop() -> std::optional<T>;
 
 private:
-  using IndicesT =
-      std::conditional_t<Mode == FixedCircularBufferMode::UNSYNCHRONIZED,
-                         internal::UnsynchronizedIndices<Capacity>, internal::SpscIndices<Capacity>>;
-
   std::array<T, Capacity> data_{};
-  IndicesT indices_;
+  std::size_t write_index_{ 0 };
+  std::size_t read_index_{ 0 };
+  std::size_t size_{ 0 };
 };
 
-template <typename T, std::size_t Capacity, FixedCircularBufferMode Mode>
-inline FixedCircularBuffer<T, Capacity, Mode>::FixedCircularBuffer(FixedCircularBuffer&& other) noexcept
-  : data_(std::move(data_)), indices_(std::exchange(other.indices_, IndicesT{})) {
+template <typename T, std::size_t Capacity>
+inline FixedCircularBuffer<T, Capacity>::FixedCircularBuffer(FixedCircularBuffer&& other) noexcept
+  : data_(std::move(data_))
+  , write_index_(std::exchange(other.write_index_, 0))
+  , read_index_(std::exchange(other.read_index_, 0))
+  , size_(std::exchange(other.size_, false)) {
 }
 
-template <typename T, std::size_t Capacity, FixedCircularBufferMode Mode>
-inline auto FixedCircularBuffer<T, Capacity, Mode>::operator=(FixedCircularBuffer&& other) noexcept
+template <typename T, std::size_t Capacity>
+inline auto FixedCircularBuffer<T, Capacity>::operator=(FixedCircularBuffer&& other) noexcept
     -> FixedCircularBuffer& {
   if (this != &other) {
     data_ = std::move(data_);
-    indices_ = std::exchange(other.indices_, IndicesT{});
+    write_index_ = std::exchange(other.write_index_, 0);
+    read_index_ = std::exchange(other.read_index_, 0);
+    size_ = std::exchange(other.size_, 0);
   }
 
   return *this;
 }
 
-template <typename T, std::size_t Capacity, FixedCircularBufferMode Mode>
-inline auto FixedCircularBuffer<T, Capacity, Mode>::empty() const -> bool {
+template <typename T, std::size_t Capacity>
+inline auto FixedCircularBuffer<T, Capacity>::empty() const -> bool {
   return indices_.empty();
 }
 
-template <typename T, std::size_t Capacity, FixedCircularBufferMode Mode>
-inline auto FixedCircularBuffer<T, Capacity, Mode>::full() const -> bool {
+template <typename T, std::size_t Capacity>
+inline auto FixedCircularBuffer<T, Capacity>::full() const -> bool {
   return indices_.full();
 }
 
-template <typename T, std::size_t Capacity, FixedCircularBufferMode Mode>
-inline auto FixedCircularBuffer<T, Capacity, Mode>::size() const -> std::size_t {
+template <typename T, std::size_t Capacity>
+inline auto FixedCircularBuffer<T, Capacity>::size() const -> std::size_t {
   return indices_.size();
 }
 
-template <typename T, std::size_t Capacity, FixedCircularBufferMode Mode>
+template <typename T, std::size_t Capacity>
 template <std::convertible_to<T> U>
-inline auto FixedCircularBuffer<T, Capacity, Mode>::push(U&& u) -> bool {
-  return indices_.push(data_, std::forward<U>(u));
+inline auto FixedCircularBuffer<T, Capacity>::push(U&& value) -> bool {
+  if (full()) {
+    return false;
+  }
+
+  data_[write_index_] = std::forward<U>(value);
+  write_index_ = (write_index_ + 1) % Capacity;
+  ++size_;
+
+  return true;
 }
 
-template <typename T, std::size_t Capacity, FixedCircularBufferMode Mode>
+template <typename T, std::size_t Capacity>
 template <std::convertible_to<T> U>
-inline auto FixedCircularBuffer<T, Capacity, Mode>::pushForce(U&& u) -> void {
-  static_assert(Mode != FixedCircularBufferMode::SPSC, "force push is not supported for SPSC");
+inline auto FixedCircularBuffer<T, Capacity>::pushForce(U&& u) -> void {
   while (!push(std::forward<U>(u))) {
     [[maybe_unused]] auto popped_element = pop();
     assert(popped_element.has_value());
   }
 }
 
-template <typename T, std::size_t Capacity, FixedCircularBufferMode Mode>
-inline auto FixedCircularBuffer<T, Capacity, Mode>::pop() -> std::optional<T> {
-  return indices_.pop(data_);
+template <typename T, std::size_t Capacity>
+inline auto FixedCircularBuffer<T, Capacity>::pop() -> std::optional<T> {
+  std::optional<T> res;
+  if (empty()) {
+    return res;
+  }
+  res.emplace(std::move(data_[read_index_]));
+  read_index_ = (read_index_ + 1) % Capacity;
+  --size_;
+
+  return res;
 }
 }  // namespace heph::containers
--- modules/containers/tests/fixed_circular_buffer_tests.cpp
+++ modules/containers/tests/fixed_circular_buffer_tests.cpp
@@ -4,7 +4,6 @@
 
 #include <cstddef>
 #include <optional>
-#include <thread>
 
 #include <gtest/gtest.h>
 
@@ -85,110 +84,6 @@ TEST(FixedCircularQueue, Push) {
     res = queue.pop();
     EXPECT_FALSE(res.has_value());
   }
-}
-TEST(FixedCircularQueue, PushSpsc) {
-  {
-    static constexpr std::size_t QUEUE_SIZE = 1;
-
-    FixedCircularBuffer<int, QUEUE_SIZE, FixedCircularBufferMode::SPSC> queue;
-    EXPECT_TRUE(queue.empty());
-    EXPECT_FALSE(queue.full());
-    EXPECT_EQ(queue.size(), 0);
-
-    EXPECT_TRUE(queue.push(0));
-    EXPECT_FALSE(queue.empty());
-    EXPECT_TRUE(queue.full());
-    EXPECT_EQ(queue.size(), 1);
-    EXPECT_FALSE(queue.push(1));
-    EXPECT_FALSE(queue.empty());
-    EXPECT_TRUE(queue.full());
-    EXPECT_EQ(queue.size(), 1);
-
-    std::optional<int> res;
-
-    res = queue.pop();
-    EXPECT_TRUE(res.has_value());
-    EXPECT_EQ(*res, 0);
-
-    EXPECT_TRUE(queue.push(1));
-    res = queue.pop();
-    EXPECT_TRUE(res.has_value());
-    EXPECT_EQ(*res, 1);
-
-    res = queue.pop();
-    EXPECT_FALSE(res.has_value());
-  }
-  {
-    static constexpr std::size_t QUEUE_SIZE = 2;
-
-    FixedCircularBuffer<int, QUEUE_SIZE, FixedCircularBufferMode::SPSC> queue;
-    EXPECT_TRUE(queue.empty());
-    EXPECT_FALSE(queue.full());
-    EXPECT_EQ(queue.size(), 0);
-
-    EXPECT_TRUE(queue.push(0));
-    EXPECT_FALSE(queue.empty());
-    EXPECT_FALSE(queue.full());
-    EXPECT_EQ(queue.size(), 1);
-    EXPECT_TRUE(queue.push(1));
-    EXPECT_FALSE(queue.empty());
-    EXPECT_TRUE(queue.full());
-    EXPECT_EQ(queue.size(), 2);
-    EXPECT_FALSE(queue.push(2));
-    EXPECT_FALSE(queue.empty());
-    EXPECT_TRUE(queue.full());
-    EXPECT_EQ(queue.size(), 2);
-
-    std::optional<int> res;
-
-    res = queue.pop();
-    EXPECT_TRUE(res.has_value());
-    EXPECT_EQ(*res, 0);
-    EXPECT_EQ(queue.size(), 1);
-
-    res = queue.pop();
-    EXPECT_TRUE(res.has_value());
-    EXPECT_EQ(*res, 1);
-    EXPECT_EQ(queue.size(), 0);
-
-    EXPECT_TRUE(queue.push(2));
-    EXPECT_EQ(queue.size(), 1);
-    res = queue.pop();
-    EXPECT_TRUE(res.has_value());
-    EXPECT_EQ(*res, 2);
-    EXPECT_EQ(queue.size(), 0);
-
-    res = queue.pop();
-    EXPECT_FALSE(res.has_value());
-  }
-}
-TEST(FixedCircularQueue, PushSpscConcurrent) {
-  static constexpr std::size_t NUMBER_OF_ITERATIONS = 100000;
-  static constexpr std::size_t QUEUE_SIZE = 2;
-
-  FixedCircularBuffer<std::size_t, QUEUE_SIZE, FixedCircularBufferMode::SPSC> queue;
-
-  std::thread producer{ [&]() {
-    for (std::size_t i = 0; i != NUMBER_OF_ITERATIONS; ++i) {
-      while (!queue.push(i)) {
-        ;  // the other thread should pop eventually
-      }
-    }
-  } };
-  std::thread consumer{ [&]() {
-    for (std::size_t i = 0; i != NUMBER_OF_ITERATIONS; ++i) {
-      while (true) {
-        auto res = queue.pop();
-        if (res.has_value()) {
-          EXPECT_EQ(res.value(), i);
-          break;
-        }
-      }
-    }
-  } };
-
-  producer.join();
-  consumer.join();
 }
 // NOLINTEND(bugprone-unchecked-optional-access)
 }  // namespace heph::containers::tests
