2f7610998a52f880fdef2008d8fcc736730003fb
Thomas Heller <heller@filics.eu>
2025-12-02 17:59:25 +0100

    Feature: Adding Channel
    Channel is an abstraction over a bounded circular buffer. It can be used
    to to asynchronously receive and send values usind the `stdexec`
    framework.

---
+++ modules/concurrency/include/hephaestus/concurrency/channel.h
@@ -0,0 +1,340 @@
+//=================================================================================================
+// Copyright (C) 2023-2025 HEPHAESTUS Contributors
+//=================================================================================================
+
+#pragma once
+
+#include <cstddef>
+#include <exception>
+#include <mutex>
+#include <optional>
+#include <thread>
+#include <type_traits>
+#include <utility>
+
+#include <absl/base/thread_annotations.h>
+#include <absl/synchronization/mutex.h>
+#include <boost/circular_buffer.hpp>
+#include <stdexec/__detail/__execution_fwd.hpp>
+#include <stdexec/execution.hpp>
+
+#include "hephaestus/containers/intrusive_fifo_queue.h"
+
+namespace heph::concurrency {
+namespace internal {
+struct AwaiterBase {
+  virtual ~AwaiterBase() = default;
+  virtual void retry() noexcept = 0;
+
+private:
+  friend struct heph::containers::IntrusiveFifoQueueAccess;
+  [[maybe_unused]] AwaiterBase* next_{ nullptr };
+  [[maybe_unused]] AwaiterBase* prev_{ nullptr };
+};
+
+using AwaiterQueueT = containers::IntrusiveFifoQueue<AwaiterBase>;
+
+template <typename T, std::size_t Capacity>
+struct GetValueSender;
+template <typename T, std::size_t Capacity>
+struct SetValueSender;
+}  // namespace internal
+
+/// Asynchronous communication channel
+///
+/// The member functions \ref getValue and \ref setValue return senders.
+/// For waiting on them, use `stdexec::sync_wait`.
+///
+/// @tparam T The type of the values to store
+/// @tparam Capacity Maximum number of elements the Channel can store
+///
+/// Example:
+/// @code
+/// Channel<int, 10> c;
+///
+/// // Using Sender/Receiver
+/// // Producer:
+/// c.setValue(42) | stdexec::then([](){ fmt::println("Value has been successfully set!"); });
+///
+/// // Consumer:
+/// c.getValue() | stdexec::then([](int res){ fmt::println("Received {}!", res); });
+///
+/// // Using `stdexec::sync_wait`
+/// // Producer:
+/// stdexec::sync_wait(c.setValue(42));
+///
+/// // Consumer:
+/// auto [res] = *stdexec::sync_wait(c.getValue());
+/// assert(res == 42);
+///
+/// // Using coroutines:
+/// // Producer:
+/// co_await c.setValue(42);
+///
+/// // Consumer:
+/// auto res = co_wait c.getValue();
+/// assert(res == 42);
+///
+/// @endcode
+template <typename T, std::size_t Capacity>
+class Channel {
+  using GetValueSender = internal::GetValueSender<T, Capacity>;
+  using SetValueSender = internal::SetValueSender<T, Capacity>;
+
+public:
+  /// Retrieve a value stored in the channel. The returned sender will complete as soon as there is
+  /// at least one item stored in the channel.
+  ///
+  /// @param t The value to send via the channel
+  [[nodiscard]] auto setValue(T t) -> SetValueSender;
+  /// Push a value into the channel. The returned sender will complete if there is space to store an
+  /// element. Otherwise blocks until at least one item was consumed. Then returned sender completes with the
+  /// value received from the channel.
+  [[nodiscard]] auto getValue() -> GetValueSender;
+
+  [[nodiscard]] auto tryGetValue() -> std::optional<T> {
+    internal::AwaiterBase* set_awaiter{ nullptr };
+    std::optional<T> res;
+    {
+      std::scoped_lock lock{ mutex_ };
+      if (data_.empty()) {
+        return res;
+      }
+      res.emplace(std::move(data_.front()));
+      data_.pop_front();
+
+      set_awaiter = set_awaiters_.dequeue();
+    }
+    if (set_awaiter != nullptr) {
+      set_awaiter->retry();
+    }
+    return res;
+  }
+
+  void setValueOverwrite(T t) {
+    internal::AwaiterBase* get_awaiter{ nullptr };
+    {
+      std::scoped_lock lock{ mutex_ };
+      if (data_.full()) {
+        data_.pop_front();
+      }
+      data_.push_back(std::move(t));
+
+      get_awaiter = get_awaiters_.dequeue();
+    }
+    if (get_awaiter != nullptr) {
+      get_awaiter->retry();
+    }
+  }
+
+private:
+  template <typename T_, std::size_t Capacity_>
+  friend struct internal::SetValueSender;
+  template <typename T_, std::size_t Capacity_>
+  friend struct internal::GetValueSender;
+
+  template <typename OnSuccess, typename OnFail>
+  void setValueImpl(T&& t, internal::AwaiterBase* set_awaiter, OnSuccess on_success, OnFail on_fail) {
+    internal::AwaiterBase* get_awaiter{ nullptr };
+    {
+      std::scoped_lock lock{ mutex_ };
+      if (data_.full()) {
+        set_awaiters_.enqueue(set_awaiter);
+        on_fail();
+        return;
+      }
+      data_.push_back(std::move(t));
+
+      get_awaiter = get_awaiters_.dequeue();
+      on_success();
+    }
+    if (get_awaiter != nullptr) {
+      get_awaiter->retry();
+    }
+  }
+
+  template <typename OnSuccess, typename OnFail>
+  void getValueImpl(internal::AwaiterBase* get_awaiter, OnSuccess on_success, OnFail on_fail) {
+    internal::AwaiterBase* set_awaiter{ nullptr };
+    {
+      std::scoped_lock lock{ mutex_ };
+      if (data_.empty()) {
+        get_awaiters_.enqueue(get_awaiter);
+        on_fail();
+        return;
+      }
+      on_success(std::move(data_.front()));
+      data_.pop_front();
+
+      set_awaiter = set_awaiters_.dequeue();
+    }
+    if (set_awaiter != nullptr) {
+      set_awaiter->retry();
+    }
+  }
+
+private:
+  mutable std::recursive_mutex mutex_;
+  boost::circular_buffer<T> data_{ Capacity };
+  internal::AwaiterQueueT set_awaiters_ ABSL_GUARDED_BY(mutex_);
+  internal::AwaiterQueueT get_awaiters_ ABSL_GUARDED_BY(mutex_);
+};
+
+namespace internal {
+template <typename T, std::size_t Capacity>
+struct SetValueSender {
+  using sender_concept = stdexec::sender_t;
+  using completion_signatures =
+      stdexec::completion_signatures<stdexec::set_value_t(), stdexec::set_stopped_t(),
+                                     stdexec::set_error_t(std::exception_ptr)>;
+
+  template <typename Receiver>
+  class Operation : public AwaiterBase {
+    struct OnStopRequested {
+      void operator()() const noexcept {
+        bool call_stop = [this]() {
+          std::scoped_lock lock{ channel->mutex_ };
+          if (channel->set_awaiters_.erase(self)) {
+            return true;
+          }
+          return false;
+        }();
+        if (call_stop) {
+          stdexec::set_stopped(std::move(self->receiver_));
+        }
+      }
+      Channel<T, Capacity>* channel;
+      Operation* self;
+    };
+    using EnvT = stdexec::env_of_t<Receiver>;
+    using StopTokenT = stdexec::stop_token_of_t<EnvT>;
+    using StopCallbackT = stdexec::stop_callback_for_t<StopTokenT, OnStopRequested>;
+
+  public:
+    Operation(Channel<T, Capacity>* self, T value, Receiver receiver)
+      : self_(self), value_(std::move(value)), receiver_{ std::move(receiver) } {
+    }
+
+    void start() noexcept {
+      bool set_value = false;
+      self_->setValueImpl(
+          std::move(value_), this, [this, &set_value]() { set_value = true; },
+          [this]() {
+            stop_callback_.emplace(stdexec::get_stop_token(stdexec::get_env(receiver_)),
+                                   OnStopRequested{ self_, this });
+          });
+      if (set_value) {
+        stdexec::set_value(std::move(receiver_));
+      }
+    }
+
+    void retry() noexcept final {
+      bool set_value = false;
+      self_->setValueImpl(
+          std::move(value_), this, [this, &set_value]() mutable { set_value = true; }, [this]() {});
+      if (set_value) {
+        stop_callback_.reset();
+        stdexec::set_value(std::move(receiver_));
+      }
+    }
+
+  private:
+    Channel<T, Capacity>* self_;
+    T value_;
+    Receiver receiver_;
+    std::optional<StopCallbackT> stop_callback_;
+  };
+
+  template <typename Receiver, typename ReceiverT = std::decay_t<Receiver>>
+  auto connect(Receiver&& receiver) && {
+    return Operation<ReceiverT>(self, std::move(value), std::forward<Receiver>(receiver));
+  }
+
+  Channel<T, Capacity>* self;
+  T value;
+};
+}  // namespace internal
+
+template <typename T, std::size_t Capacity>
+auto Channel<T, Capacity>::setValue(T t) -> internal::SetValueSender<T, Capacity> {
+  return internal::SetValueSender<T, Capacity>{ this, std::move(t) };
+}
+
+namespace internal {
+template <typename T, std::size_t Capacity>
+struct GetValueSender {
+  using sender_concept = stdexec::sender_t;
+  using completion_signatures =
+      stdexec::completion_signatures<stdexec::set_value_t(T), stdexec::set_stopped_t(),
+                                     stdexec::set_error_t(std::exception_ptr)>;
+
+  template <typename Receiver>
+  class Operation : public AwaiterBase {
+    struct OnStopRequested {
+      void operator()() const noexcept {
+        bool call_stop = [this]() {
+          std::scoped_lock lock{ channel->mutex_ };
+          if (channel->get_awaiters_.erase(self)) {
+            return true;
+          }
+          return false;
+        }();
+        if (call_stop) {
+          stdexec::set_stopped(std::move(self->receiver_));
+        }
+      }
+      Channel<T, Capacity>* channel;
+      Operation* self;
+    };
+    using EnvT = stdexec::env_of_t<Receiver>;
+    using StopTokenT = stdexec::stop_token_of_t<EnvT>;
+    using StopCallbackT = stdexec::stop_callback_for_t<StopTokenT, OnStopRequested>;
+
+  public:
+    Operation(Channel<T, Capacity>* self, Receiver receiver) : self_(self), receiver_{ std::move(receiver) } {
+    }
+
+    void start() noexcept {
+      std::optional<T> value;
+      self_->getValueImpl(
+          this, [this, &value](T&& t) { value.emplace(std::move(t)); },
+          [this]() {
+            stop_callback_.emplace(stdexec::get_stop_token(stdexec::get_env(receiver_)),
+                                   OnStopRequested{ self_, this });
+          });
+      if (value.has_value()) {
+        stdexec::set_value(std::move(receiver_), std::move(*value));
+        return;
+      }
+    }
+
+    void retry() noexcept final {
+      std::optional<T> value;
+      self_->getValueImpl(this, [this, &value](T&& t) mutable { value.emplace(std::move(t)); }, [this]() {});
+      if (value.has_value()) {
+        stop_callback_.reset();
+        stdexec::set_value(std::move(receiver_), std::move(*value));
+      }
+    }
+
+  private:
+    Channel<T, Capacity>* self_;
+    Receiver receiver_;
+    std::optional<StopCallbackT> stop_callback_;
+  };
+
+  template <typename Receiver, typename ReceiverT = std::decay_t<Receiver>>
+  auto connect(Receiver&& receiver) && {
+    return Operation<ReceiverT>(self, std::forward<Receiver>(receiver));
+  }
+
+  Channel<T, Capacity>* self;
+};
+}  // namespace internal
+
+template <typename T, std::size_t Capacity>
+auto Channel<T, Capacity>::getValue() -> internal::GetValueSender<T, Capacity> {
+  return internal::GetValueSender<T, Capacity>{ this };
+}
+
+}  // namespace heph::concurrency
---
+++ modules/concurrency/tests/channel_tests.cpp
@@ -0,0 +1,179 @@
+//=================================================================================================
+// Copyright (C) 2023-2025 HEPHAESTUS Contributors
+//=================================================================================================
+
+#include <chrono>
+#include <cstddef>
+#include <thread>
+
+#include <exec/async_scope.hpp>
+#include <exec/when_any.hpp>
+#include <gtest/gtest.h>
+#include <stdexec/execution.hpp>
+
+#include "hephaestus/concurrency/channel.h"
+// NOLINTBEGIN(bugprone-unchecked-optional-access)
+namespace heph::concurrency {
+TEST(Channel, SendRecv) {
+  Channel<int, 2> channel;
+
+  {
+    auto [value] = *stdexec::sync_wait(stdexec::when_all(channel.getValue(), channel.setValue(1)));
+
+    EXPECT_EQ(value, 1);
+  }
+  {
+    auto [value] = *stdexec::sync_wait(stdexec::when_all(channel.setValue(1), channel.getValue()));
+
+    EXPECT_EQ(value, 1);
+  }
+}
+
+TEST(Channel, SendCancel) {
+  Channel<int, 1> channel;
+  // set once to make channel block later on
+  stdexec::sync_wait(channel.setValue(0));
+
+  auto [res] =
+      *stdexec::sync_wait(exec::when_any(stdexec::just(true), channel.setValue(0) | stdexec::then([]() {
+                                                                EXPECT_TRUE(false);
+                                                                return false;
+                                                              })));
+
+  EXPECT_TRUE(res);
+}
+
+TEST(Channel, GetCancel) {
+  Channel<int, 2> channel;
+
+  auto [res] = *stdexec::sync_wait(exec::when_any(
+      stdexec::just(true), channel.getValue() | stdexec::then([](auto /*unused*/) { return false; })));
+
+  EXPECT_TRUE(res);
+}
+
+TEST(Channel, SendRecvOrder) {
+  Channel<int, 4> channel;
+
+  stdexec::sync_wait(channel.setValue(0));
+  stdexec::sync_wait(channel.setValue(1));
+  stdexec::sync_wait(channel.setValue(2));
+  stdexec::sync_wait(channel.setValue(3));
+
+  {
+    auto [value] = *stdexec::sync_wait(channel.getValue());
+    EXPECT_EQ(value, 0);
+  }
+  {
+    auto [value] = *stdexec::sync_wait(channel.getValue());
+    EXPECT_EQ(value, 1);
+  }
+  {
+    auto [value] = *stdexec::sync_wait(channel.getValue());
+    EXPECT_EQ(value, 2);
+  }
+  {
+    auto [value] = *stdexec::sync_wait(channel.getValue());
+    EXPECT_EQ(value, 3);
+  }
+  stdexec::sync_wait(channel.setValue(1));
+  stdexec::sync_wait(channel.setValue(2));
+  {
+    auto [value] = *stdexec::sync_wait(channel.getValue());
+    EXPECT_EQ(value, 1);
+  }
+  {
+    auto [value] = *stdexec::sync_wait(channel.getValue());
+    EXPECT_EQ(value, 2);
+  }
+}
+
+TEST(Channel, SendRecvParallel) {
+  Channel<std::size_t, 4> channel;
+  static constexpr std::size_t NUMBER_OF_ITERATIONS = 10000;
+
+  std::thread producer{ [&]() {
+    for (std::size_t i = 0; i != NUMBER_OF_ITERATIONS; ++i) {
+      stdexec::sync_wait(channel.setValue(i));
+    }
+  } };
+  std::thread consumer{ [&]() {
+    for (std::size_t i = 0; i != NUMBER_OF_ITERATIONS; ++i) {
+      auto [res] = *stdexec::sync_wait(channel.getValue());
+      EXPECT_EQ(res, i);
+    }
+  } };
+
+  producer.join();
+  consumer.join();
+}
+
+TEST(Channel, SendRecvParallelScope) {
+  Channel<std::size_t, 4> channel;
+  static constexpr std::size_t NUMBER_OF_ITERATIONS = 10000;
+
+  std::thread producer{ [&]() {
+    exec::async_scope scope;
+    for (std::size_t i = 0; i != NUMBER_OF_ITERATIONS; ++i) {
+      scope.spawn(channel.setValue(i));
+    }
+    stdexec::sync_wait(scope.on_empty());
+  } };
+  std::thread consumer{ [&]() {
+    exec::async_scope scope;
+    absl::Mutex mutex;
+    std::set<int> received_values;
+    for (std::size_t i = 0; i != NUMBER_OF_ITERATIONS; ++i) {
+      scope.spawn(channel.getValue() | stdexec::then([&received_values, &mutex](int value) {
+                    // The continuation might run on the produce thread...
+                    absl::MutexLock lock{ &mutex };
+                    auto [_, inserted] = received_values.insert(value);
+                    EXPECT_TRUE(inserted);
+                  }));
+    }
+    stdexec::sync_wait(scope.on_empty());
+    EXPECT_EQ(received_values.size(), NUMBER_OF_ITERATIONS);
+  } };
+
+  producer.join();
+  consumer.join();
+}
+
+TEST(Channel, SendRecvParallelScopeStop) {
+  Channel<std::size_t, 4> channel;
+  static constexpr std::size_t NUMBER_OF_ITERATIONS = 10000;
+
+  std::thread producer{ [&]() {
+    exec::async_scope scope;
+    for (std::size_t i = 0; i != NUMBER_OF_ITERATIONS; ++i) {
+      scope.spawn(channel.setValue(i));
+    }
+    // Just give a little leeway to have some executed...
+    std::this_thread::sleep_for(std::chrono::microseconds(1));
+    scope.request_stop();
+    stdexec::sync_wait(scope.on_empty());
+  } };
+  std::thread consumer{ [&]() {
+    exec::async_scope scope;
+    absl::Mutex mutex;
+    std::set<int> received_values;
+    for (std::size_t i = 0; i != NUMBER_OF_ITERATIONS; ++i) {
+      scope.spawn(channel.getValue() | stdexec::then([&received_values, &mutex](int value) {
+                    // The continuation might run on the produce thread...
+                    absl::MutexLock lock{ &mutex };
+                    auto [_, inserted] = received_values.insert(value);
+                    EXPECT_TRUE(inserted);
+                  }));
+    }
+    // Just give a little leeway to have some executed...
+    std::this_thread::sleep_for(std::chrono::microseconds(1));
+    scope.request_stop();
+    stdexec::sync_wait(scope.on_empty());
+  } };
+
+  producer.join();
+  consumer.join();
+}
+
+}  // namespace heph::concurrency
+// NOLINTEND(bugprone-unchecked-optional-access)
