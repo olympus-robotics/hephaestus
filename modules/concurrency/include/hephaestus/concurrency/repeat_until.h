6c84873ab073b562be5a4b667fab18810a0ff5bc
Thomas Heller <thom.heller@gmail.com>
2025-12-04 18:00:28 +0100

    Chore: Cleanup repeatUntil (#450)
    Using exec::repeat_until_effect under the hood to simplify code.
    
    
    ## Type of change
    
    - Bug fix (non-breaking change which fixes an issue)
    
    
    ## Checklist before requesting a review
    - [x] I have performed a self-review of my code.
    - [x] If it is a core feature, I have added thorough tests.
    - [x] If this is a new component I have added examples.
    - [x] I updated the README and related documentation.

--- modules/concurrency/include/hephaestus/concurrency/repeat_until.h
+++ modules/concurrency/include/hephaestus/concurrency/repeat_until.h
@@ -4,118 +4,43 @@
 
 #pragma once
 
-#include <exception>
-#include <type_traits>
 #include <utility>
 
-#include <stdexec/__detail/__execution_fwd.hpp>
-#include <stdexec/__detail/__optional.hpp>
-#include <stdexec/__detail/__sender_introspection.hpp>
+#include <exec/repeat_effect_until.hpp>
 #include <stdexec/__detail/__senders_core.hpp>
 #include <stdexec/execution.hpp>
 
-#include "hephaestus/concurrency/basic_sender.h"
-
 namespace heph::concurrency {
-
 namespace internal {
+
 template <typename SenderFactoryT>
 concept SenderFactory = requires(SenderFactoryT sender_factory) {
   { sender_factory() } -> stdexec::sender;
 };
 }  // namespace internal
 
-struct RepeatUntilT {
-  template <stdexec::sender SenderT>
-  auto operator()(SenderT&& sender) const {
-    return (*this)([sender = std::forward<SenderT>(sender)] { return sender; });
-  }
+/// repeatedly starts \param sender_factory until the completion of the sender returns `true`.
+///
+/// \param sender_factory nullary function object returning a sender with a bool completion set_value
+/// completion.
+///
+/// This can be used to implement loops using sender receivers. By passing a factory instead of the sender
+/// directly, we are able to use movable only senders.
+template <internal::SenderFactory SenderFactoryT>
+auto repeatUntil(SenderFactoryT&& factory) {
+  // 1. Create a lazy generator that calls the factory
+  return stdexec::just() |
+         stdexec::let_value([factory = std::forward<SenderFactoryT>(factory)]() mutable {
+           // 2. Call the factory and normalize errors *inside* the loop
+           return factory();
+         })
+         // 3. Repeat until the sender returns 'true'
+         | exec::repeat_effect_until();
+}
 
-  template <internal::SenderFactory SenderFactoryT>
-  auto operator()(SenderFactoryT&& sender_factory) const {
-    return concurrency::makeSenderExpression<RepeatUntilT>(std::forward<SenderFactoryT>(sender_factory));
-  }
-};
-
-// NOLINTNEXTLINE(readability-identifier-naming)
-inline constexpr RepeatUntilT repeatUntil{};
-
-namespace internal {
-template <typename SenderFactoryT, typename ReceiverT>
-struct RepeatUntilStateT {
-  using SenderT = std::invoke_result_t<SenderFactoryT>;
-  struct InnerReceiverT {
-    RepeatUntilStateT* self;
-    using receiver_concept = stdexec::receiver_t;
-
-    // NOLINTNEXTLINE(readability-identifier-naming)
-    void set_value(bool done) const noexcept {
-      if (done) {
-        stdexec::set_value(std::move(self->receiver));
-        return;
-      }
-      self->start();
-    }
-
-    // NOLINTNEXTLINE(readability-identifier-naming)
-    void set_stopped() const noexcept {
-      stdexec::set_stopped(std::move(self->receiver));
-    }
-
-    template <typename Error>
-    // NOLINTNEXTLINE(readability-identifier-naming)
-    void set_error(Error error) const noexcept {
-      if constexpr (std::is_same_v<std::decay_t<Error>, std::exception_ptr>) {
-        stdexec::set_error(std::move(self->receiver), std::move(error));
-      } else {
-        try {
-          throw error;
-        } catch (...) {
-          stdexec::set_error(std::move(self->receiver), std::current_exception());
-        }
-      }
-    }
-
-    // NOLINTNEXTLINE(readability-identifier-naming)
-    auto get_env() const noexcept {
-      return stdexec::get_env(self->receiver);
-    }
-  };
-  SenderFactoryT sender_factory;
-  ReceiverT receiver;
-  stdexec::__optional<stdexec::connect_result_t<SenderT, InnerReceiverT>> state;
-
-  void start() {
-    try {
-      state.emplace(stdexec::__emplace_from{
-          [this]() { return stdexec::connect(sender_factory(), InnerReceiverT{ this }); } });
-
-    } catch (...) {
-      stdexec::set_error(std::move(receiver), std::current_exception());
-      return;
-    }
-    stdexec::start(*state);
-  }
-};
-
-}  // namespace internal
-
-template <>
-struct SenderExpressionImpl<RepeatUntilT> : DefaultSenderExpressionImpl {
-  static constexpr auto GET_COMPLETION_SIGNATURES = []<typename Sender>(
-                                                        Sender&&, heph::concurrency::Ignore = {}) noexcept {
-    return stdexec::completion_signatures<stdexec::set_value_t(), stdexec::set_error_t(std::exception_ptr),
-                                          stdexec::set_stopped_t()>{};
-  };
-  static constexpr auto GET_STATE = []<typename Sender, typename Receiver>(Sender&& sender,
-                                                                           Receiver&& receiver) noexcept {
-    auto [_, sender_factory] = std::forward<Sender>(sender);
-    using SenderFactoryT = std::decay_t<stdexec::__data_of<Sender>>;
-    using ReceivertT = std::decay_t<Receiver>;
-    return internal::RepeatUntilStateT<SenderFactoryT, ReceivertT>{ std::move(sender_factory),
-                                                                    std::forward<Receiver>(receiver) };
-  };
-  static constexpr auto START = []<typename Receiver>(auto& self, Receiver&& /*receiver*/) { self.start(); };
-};
-
+/// Convenience wrapper for \ref repeatUntil for cases where \param sender is copyable
+template <stdexec::sender SenderT>
+auto repeatUntil(SenderT&& sender) {
+  return std::forward<SenderT>(sender) | exec::repeat_effect_until();
+}
 }  // namespace heph::concurrency
